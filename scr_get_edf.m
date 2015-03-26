a73826a9b772cc1589ec72454f63b1a4ef8914bb
Tobias Moser <tmoser@mycompany.com>
2015-03-26 14:32:37 +0000

    support for edf data format
    git-svn-id: https://svn.code.sf.net/p/pspm/svn/trunk@28 23ec9670-3a1e-4a12-9187-a0ba8bce340d

--- scr_get_edf.m
+++ scr_get_edf.m
@@ -1,5 +1,5 @@
 function [sts, import, sourceinfo] = scr_get_edf(datafile, import)
-% scr_get_brainvis is the main function for import of EDF files
+% scr_get_edf is the main function for import of EDF files
 % FORMAT: [sts, import, sourceinfo] = scr_get_edf(datafile, import);
 % this function uses fieldtrip fileio functions
 %__________________________________________________________________________
@@ -8,9 +8,6 @@ function [sts, import, sourceinfo] = scr_get_edf(datafile, import)
 
 % $Id$
 % $Rev$
-
-% NOTE I did not have sample files, simply assumed that hdr.labels would be
-% a cell array (copied from scr_get_brainvis)
 
 % initialise
 % -------------------------------------------------------------------------
@@ -23,42 +20,34 @@ addpath([settings.path, 'Import', filesep, 'fieldtrip']);
 % -------------------------------------------------------------------------
 hdr = ft_read_header(datafile);
 indata = ft_read_data(datafile);
-try mrk = ft_read_event(datafile); catch, mrk = []; end;
+
+% convert 3 dim to 2 dim (summarize all trials)
+if numel(size(indata)) == 3,
+    indata = indata(:,:);
+end;
 
 % extract individual channels
 % -------------------------------------------------------------------------
 % loop through import jobs
 for k = 1:numel(import)
-        
-    if strcmpi(settings.chantypes(import{k}.typeno).data, 'wave')
-        % define channel number ---
-        if import{k}.channel > 0
-            chan = import{k}.channel;
-        else
-            chan = scr_find_channel(hdr.label, import{k}.type);
-            if chan < 1, return; end;
-        end;
-        
-        if chan > numel(hdr.label), warning('ID:channel_not_contained_in_file', 'Channel %02.0f not contained in file %s.\n', chan, datafile); return; end;
-        
-        sourceinfo.chan{k, 1} = sprintf('Channel %02.0f: %s', chan, hdr.label{chan});
-        
-        % sample rate ---
-        import{k}.sr = hdr.Fs;
-        
-        % get data
-        import{k}.data = indata(chan, :);
-    else 
-        % marker channels: get the ascending flank of each marker
-        sourceinfo.chan{k, 1} = 'Automatically extracted marker recordings';
-        % time unit
-        import{k}.sr = 1./hdr.Fs;
-        import{k}.marker = 'timestamps';
-        import{k}.data = [mrk.sample];
-        import{k}.markerinfo.value = [mrk.value];
-        import{k}.markerinfo.name  = {mrk.type};
-    end;
        
+    chan = import{k}.channel;
+    if chan > numel(hdr.label), 
+        warning('ID:channel_not_contained_in_file', 'Channel %02.0f not contained in file %s.\n', chan, datafile); 
+        return; 
+    end;
+    % data
+    import{k}.data = indata(chan);     % data per channel
+    % sample rate ---
+    import{k}.sr = hdr.Fs;
+    sourceinfo.chan{k, 1} = sprintf('Channel %02.0f: %s', chan, hdr.label{chan});
+    
+    if strcmpi(settings.chantypes(import{k}.typeno).data, 'events')
+        % marker channels: get the ascending flank of each marker
+        sourceinfo.chan{k, 1} = 'Extracted marker recordings';
+        import{k}.marker = 'continuous';
+    end;
+           
 end;
 
 % clear path and return
