3a42aa7a90e0ec61d392fa85d97f4e41223bab9d
Samuel Gerster <sgerster@mycompany.com>
2016-06-30 12:57:58 +0000

    added options: roi, expectedSoundCount, mindelay
    git-svn-id: https://svn.code.sf.net/p/pspm/svn/trunk@317 23ec9670-3a1e-4a12-9187-a0ba8bce340d

--- scr_find_sounds.m
+++ scr_find_sounds.m
@@ -19,9 +19,12 @@ function [sts, infos] = scr_find_sounds(file, options)
 %               and displays the mean delay and standard deviation and
 %               removes triggers which could not be assigned to a trigger
 %               from existing trigger channel.
-%           maxdelay : [integer] Size of the window in seconds in which 
-%               scr_find_sounds will accept sounds to belong to a marker.
-%               default is 3s.
+%           maxdelay : [number] Upper limit (in seconds) of the window in 
+%               which scr_find_sounds will accept sounds to belong to a
+%               marker. default is 3s.
+%           mindealy : [number] Lower limit (in seconds) of the window in 
+%               which scr_find_sounds will accept sounds to belong to a
+%               marker. default is 0s.
 %           plot : [true/FALSE] displays a histogramm of the 
 %               delays found and a plot with the detected sound, the
 %               trigger and the onset of the sound events. These are color
@@ -36,6 +39,10 @@ function [sts, infos] = scr_find_sounds(file, options)
 %                   waves all respecting condition 1
 %               Resampling will restore more or less the original signal
 %               and lead to more accurate timings.
+%           roi : [vector of 2 floats] Region of interest for discovering
+%               sounds. Especially usefull if pairing events with triggers.
+%               Only sounds included inbetween the 2 timestamps will be
+%               considered.
 %           sndchannel : [integer] number of the channel holding the sound.
 %               By default first 'snd' channel.
 %           threshold : [0...1] percent of the max of the power in the
@@ -43,6 +50,13 @@ function [sts, infos] = scr_find_sounds(file, options)
 %               0.1.
 %           trigchannel : [integer] number of the channel holding the 
 %               triggers. By default first 'marker' channel.
+%
+%           EXPERIMENTAL, use with caution!
+%           expectedSoundCount : Checks for correct number of
+%               detected sounds. If too few are found, lowers threshhold
+%               until at least specified cound is reached. Thresh is
+%               lowered by .01 until 0.05 is reached for a max of 95
+%               itterations.
 %   Outputs
 %       sts : 1 on successfull completion, -1 otherwise
 %       info: struct()
@@ -62,7 +76,7 @@ function [sts, infos] = scr_find_sounds(file, options)
 global settings;
 if isempty(settings), scr_init; end;
 
-sts = -1;
+sts=1;
 % Check argument
 if ~exist(file,'file')
     warning('ID:file_not_found', 'File %s was not found. Aborted.',file); return;
@@ -72,45 +86,56 @@ fprintf('Processing sound in file %s\n',file);
 
 % Process options
 try options.addchannel; catch, options.addchannel = false; end;
+try options.channeloutput; catch; options.channeloutput = 'all'; end;
 try options.diagnostics; catch, options.diagnostics = true; end;
 try options.maxdelay; catch, options.maxdelay = 3; end;
+try options.mindelay; catch, options.mindelay = 0; end;
 try options.plot; catch, options.plot = false; end;
 try options.resample; catch, options.resample = 1; end;
+try options.roi; catch, options.roi = []; end;
 try options.sndchannel; catch, options.sndchannel = 0; end;
 try options.threshold; catch, options.threshold = 0.1; end;
 try options.trigchannel; catch, options.trigchannel = 0; end;
-try options.channeloutput; catch; options.channeloutput = 'all'; end;
+try options.expectedSoundCount; catch; options.expectedSoundCount = 0; end;
 
 if options.plot
     options.diagnostics = true;
 end
 
 if ~isnumeric(options.resample) || mod(options.resample,1) || options.resample<1
-    warning('ID:invalid_input', 'Option resample is not an integer or negative.'); return;
+    warning('ID:invalid_input', 'Option resample is not an integer or negative.'); sts=-1; return;
+elseif ~isnumeric(options.mindelay) || options.mindelay < 0
+    warning('ID:invalid_input', 'Option mindelay is not a number or negative.'); sts=-1; return;
 elseif ~isnumeric(options.maxdelay) || options.maxdelay < 0
-    warning('ID:invalid_input', 'Option maxdelay is not a number or negative.'); return;
+    warning('ID:invalid_input', 'Option maxdelay is not a number or negative.'); sts=-1; return;
+elseif options.maxdelay < options.mindelay
+    warning('ID:invalid_input','mindelay is larger than mindelay.'); sts=-1; return;
 elseif ~isnumeric(options.threshold) || options.threshold < 0
-    warning('ID:invalid_input', 'Option threshold is not a number or negative.'); return;
+    warning('ID:invalid_input', 'Option threshold is not a number or negative.'); sts=-1; return;
 elseif ~isnumeric(options.sndchannel) || mod(options.sndchannel,1) || options.sndchannel < 0
-    warning('ID:invalid_input', 'Option sndchannel is not an integer.'); return;
+    warning('ID:invalid_input', 'Option sndchannel is not an integer.'); sts=-1; return;
 elseif ~isnumeric(options.trigchannel) || mod(options.trigchannel,1) || options.trigchannel < 0
-    warning('ID:invalid_input', 'Option trichannel is not an integer.'); return;
+    warning('ID:invalid_input', 'Option trichannel is not an integer.'); sts=-1; return;
 elseif ~islogical(options.addchannel) && ~isnumeric(options.addchannel)
-    warning('ID:invalid_input', 'Option addchannel is not numeric or logical'); return;
+    warning('ID:invalid_input', 'Option addchannel is not numeric or logical'); sts=-1; return;
 elseif ~islogical(options.diagnostics) && ~isnumeric(options.diagnostics)
-    warning('ID:invalid_input', 'Option diagnostics is not numeric or logical'); return;
+    warning('ID:invalid_input', 'Option diagnostics is not numeric or logical'); sts=-1; return;
 elseif ~islogical(options.plot) && ~isnumeric(options.plot)
-    warning('ID:invalid_input', 'Option plot is not numeric or logical'); return;
+    warning('ID:invalid_input', 'Option plot is not numeric or logical'); sts=-1; return;
 elseif ~strcmpi(options.channeloutput, 'all') && ~strcmpi(options.channeloutput, 'corrected')
-    warning('ID:invalid_input', 'Option channeloutput must be either ''all'' or ''corrected''.');
-    return;
+    warning('ID:invalid_input', 'Option channeloutput must be either ''all'' or ''corrected''.'); sts=-1; return;
+elseif ~isnumeric(options.expectedSoundCount) || mod(options.expectedSoundCount,1)
+    warning('ID:invalid_input', 'Option expectedSoundCount is not an integer.'); sts=-1; return;
+elseif ~isempty(options.roi) && (~length(options.roi)== 2 || ~all(isnumeric(options.roi)))
+    warning('ID:invalid_input', 'Option roi must be a float vector of length 2 or 0'); sts=-1; return;
 end
 
 % call it outinfos not to get confused
 outinfos = struct();
 
+
 % Load Data
-[sts, ininfo, indata] = scr_load_data(file);
+[sts, ~, indata] = scr_load_data(file);
 if sts == -1
     warning('ID:invalid_input', 'Failed loading file %s.', file); return;
 end;
@@ -143,79 +168,109 @@ else
     snd_pow = snd.data.^2;
 end;
 % Apply simple bidirectional square filter
+snd_pow = snd_pow-min(snd_pow);
 mask = ones(round(.01*snd.header.sr),1)/round(.01*snd.header.sr);
 snd_pow = conv(snd_pow,mask);
 snd_pow = sqrt(snd_pow(1:end-length(mask)+1).*snd_pow(length(mask):end));
 
-%% Find sound events
-thresh = max(snd_pow)*options.threshold;
-snd_pres(snd_pow>thresh) = 1;
-snd_pres(snd_pow<=thresh) = 0;
-% Convert detected sounds into events. If pulses are separated by less than
-% 50ms, combine into one event.
-mask = ones(round(0.05*snd.header.sr*options.resample),1);
-n_pad = length(mask)-1;
-c = conv(snd_pres,mask)>0;
-snd_pres = (c(1:end-n_pad) & c(n_pad+1:end));
-% Find rising and falling edges
-snd_re = t(conv([1,-1],snd_pres(1:end-1)+0)>0);
-% Find falling edges
-snd_fe = t(conv([1,-1],snd_pres(1:end-1)+0)<0);
-if numel(snd_re) ~= 0 && numel(snd_fe) ~= 0
-    % Start with a rising and end with a falling edge
-    if snd_re(1)>snd_fe(1)
-        snd_re = snd_re(2:end);
-    end
-    if snd_fe(end) < snd_re(end)
-        snd_fe = snd_fe(1:end-1);
-    end
+%% Process roi option
+if isempty(options.roi)
+    ll = 1;
+    ul = length(snd.data);
+else
+    ll = dsearchn(t,options.roi(1));
+    ul = dsearchn(t,options.roi(2));
 end
-% Discard sounds shorter than 10ms
-noevent_i = find((snd_fe-snd_re)<0.01);
-snd_re(noevent_i)=[];
-snd_fe(noevent_i)=[];
+roi_mask = false(size(snd.data));
+roi_mask(ll:ul) = true;
+loc_snd_pow = snd_pow;
+loc_snd_pow(~roi_mask) = 0;
 
-% keep current snd_re for channeloutput 'all'
-snd_re_all = snd_re;
-snd_fe_all = snd_fe;
 
-%% Triggers
-if options.diagnostics
-    % Check for existence of marker channel
-    if ~options.trigchannel
-        mkri = find(strcmpi(cellfun(@(x) x.header.chantype,indata,'un',0),'marker'),1);
-        if ~any(mkri)
-            warning('ID:no_marker_chan', 'No marker channel found. Aborted'); sts=-1; return;
+%% Find sound events
+searchForMoreSounds = true;
+while searchForMoreSounds == true
+    clear snd_pres
+    thresh = max(loc_snd_pow)*options.threshold;
+    snd_pres(loc_snd_pow>thresh) = 1;
+    snd_pres(loc_snd_pow<=thresh) = 0;
+    % Convert detected sounds into events. If pulses are separated by less than
+    % 50ms, combine into one event.
+    mask = ones(round(0.05*snd.header.sr*options.resample),1);
+    n_pad = length(mask)-1;
+    c = conv(snd_pres,mask)>0;
+    snd_pres = (c(1:end-n_pad) & c(n_pad+1:end));
+    % Find rising and falling edges
+    snd_re = t(conv([1,-1],snd_pres(1:end-1)+0)>0);
+    % Find falling edges
+    snd_fe = t(conv([1,-1],snd_pres(1:end-1)+0)<0);
+    if numel(snd_re) ~= 0 && numel(snd_fe) ~= 0
+        % Start with a rising and end with a falling edge
+        if snd_re(1)>snd_fe(1)
+            snd_re = snd_re(2:end);
         end
-    elseif options.trigchannel > numel(indata)
-        warning('ID:out_of_range', 'Option trigchannel is out of the data range.'); return;     
+        if snd_fe(end) < snd_re(end)
+            snd_fe = snd_fe(1:end-1);
+        end
+    end
+    % Discard sounds shorter than 10ms
+    noevent_i = find((snd_fe-snd_re)<0.01);
+    snd_re(noevent_i)=[];
+    snd_fe(noevent_i)=[];
+
+    % keep current snd_re for channeloutput 'all'
+    snd_re_all = snd_re;
+    snd_fe_all = snd_fe;
+
+    %% Triggers
+    if options.diagnostics
+        % Check for existence of marker channel
+        if ~options.trigchannel
+            mkri = find(strcmpi(cellfun(@(x) x.header.chantype,indata,'un',0),'marker'),1);
+            if ~any(mkri)
+                warning('ID:no_marker_chan', 'No marker channel found. Aborted'); sts=-1; return;
+            end
+        elseif options.trigchannel > numel(indata)
+            warning('ID:out_of_range', 'Option trigchannel is out of the data range.'); sts=-1; return;     
+        else
+            mkri=options.trigchannel;
+        end
+        mkr = indata{mkri};
+
+        %% Estimate delays from trigger to sound
+        delays = nan(length(mkr.data),1);
+        snd_markers = nan(length(mkr.data),1);
+        for i=1:length(mkr.data)
+            % Find sound onset in region of interest
+            t_re = snd_re(find(snd_re>mkr.data(i)+options.mindelay,1));
+            delay = t_re-mkr.data(i);
+            if delay<options.maxdelay
+                delays(i) = delay;
+                snd_markers(i)=t_re;
+            end
+        end
+        delays(isnan(delays)) = [];
+        snd_markers(isnan(snd_markers)) = [];
+        % Discard any sound event not related to a trigger
+        snd_fe = snd_fe(dsearchn(snd_re,snd_markers));
+        snd_re = snd_re(dsearchn(snd_re,snd_markers));
+        %% Display some diagnostics
+        fprintf('%4d sound events associated with a marker found\nMean Delay : %5.1f ms\nStd dev    : %5.1f ms\n',...
+            length(snd_markers),mean(delays)*1000,std(delays)*1000);
+
+        outinfos.delays = delays;
+        outinfos.snd_markers = snd_markers;
+    end
+    if length(snd_re)>=options.expectedSoundCount
+        searchForMoreSounds = false;
+    elseif options.threshold < .05
+        searchForMoreSounds = false;
+        warning('ID:MaxItteration','Not enough sounds cout be detected to match expectedSoundCount option, result is incomplete');sts=2;
     else
-        mkri=options.trigchannel;
-    end
-    mkr = indata{mkri};
-
-    %% Estimate delays from trigger to sound
-    delays = nan(length(mkr.data),1);
-    snd_markers = nan(length(mkr.data),1);
-    for i=1:length(mkr.data)
-        tr = snd_re(find(snd_re>mkr.data(i),1));
-        delay = tr-mkr.data(i);
-        if delay<options.maxdelay
-            delays(i) = delay;
-            snd_markers(i)=tr;
-        end
-    end
-    delays(isnan(delays)) = [];
-    snd_markers(isnan(snd_markers)) = [];
-    % Discard any sound event not related to a trigger
-    snd_fe = snd_fe(dsearchn(snd_re,snd_markers));
-    snd_re = snd_re(dsearchn(snd_re,snd_markers));
-    %% Display some diagnostics
-    fprintf('%4d sound events associated with a marker found\nMean Delay : %5.1f ms\nStd dev    : %5.1f ms\n',...
-        length(snd_markers),mean(delays)*1000,std(delays)*1000);
-    
-    outinfos.delays = delays;
-    outinfos.snd_markers = snd_markers;
+        options.threshold = options.threshold - 0.01;
+        warning('ID:BadData',sprintf('Only %d sounds detected but %d expected. threshold lowered to %3.2f',...
+            length(snd_re),options.expectedSoundCount,options.threshold));sts=2;
+    end  
 end
 
 %% Save as new channel
@@ -234,13 +289,19 @@ if options.addchannel
     snd_events.header.sr = 1;
     snd_events.header.chantype = 'marker';
     snd_events.header.units ='events';
-    [sts, ininfos] = scr_write_channel(file, snd_events, 'add');
+    [~, ininfos] = scr_write_channel(file, snd_events, 'add');
     outinfos.channel = ininfos.channel;
 end
 
 %% Plot Option
 if options.plot
-    figure
+    % Histogramm
+    fh = findobj('Tag','delays_hist');
+    if isempty(fh)
+        fh=figure('Tag','delays_hist');
+    else
+        figure(fh)
+    end
     % use version dependent histogram function
     if verLessThan('matlab', '8.4')
         hist(delays*1000,10)
@@ -254,7 +315,14 @@ if options.plot
         t = t(1:options.resample:end);
         snd_pres = snd_pres(1:options.resample:end);
     end
-    figure
+    % Time series
+    fh = findobj('Tag','delays_time_series');
+    if isempty(fh)
+        fh=figure('Tag','delays_time_series');
+    else
+        figure(fh)
+    end
+    
     plot(t,snd_pres)
     hold on
     scatter(mkr.data,ones(size(mkr.data))*.1,'k')
@@ -267,6 +335,5 @@ if options.plot
 end
 
 infos = outinfos;
-sts=1;
 
 end
