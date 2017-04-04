fd9db659d9979e72de0584bf31e57d3620973b7a
Tobias Moser <tmoser@mycompany.com>
2017-04-04 08:12:32 +0000

    remove simplification; bacause will not be supported in future matlab releases
    git-svn-id: https://svn.code.sf.net/p/pspm/svn/trunk@436 23ec9670-3a1e-4a12-9187-a0ba8bce340d

--- pspm_dcm.m
+++ pspm_dcm.m
@@ -590,7 +590,15 @@ for iSn = 1:numel(model.datafile)
             [dcm.sn{sn_sbs(isbSn)}.a(offset_trl).s]'];
         
         fix_stats = cell2mat({dcm.sn{sn_sbs(isbSn)}.e(offset_trl).a}');
-        dcm.stats(sbs_trl + cTrl, :) = [flex_stats, fix_stats];
+    
+        if ~isempty(fix_stats) && ~isempty(flex_stats)
+            dcm.stats(sbs_trl + cTrl, :) = [flex_stats, fix_stats];
+        elseif ~isempty(fix_stats)
+            dcm.stats(sbs_trl + cTrl, :) = fix_stats;
+        elseif ~isempty(flex_stats)
+            dcm.stats(sbs_trl + cTrl, :) = flex_stats;
+        end;
+        
     end;
     % set disabled trials to NaN
     dcm.stats(cTrl + find(trls(:, 1) == 0), :) = NaN;
