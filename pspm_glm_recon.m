2477e5fecb1956b29480307f55c2bc852407a18d
Tobias Moser <tmoser@mycompany.com>
2017-07-03 09:27:18 +0000

    commit bugfix fro pspm_glm_recon
    git-svn-id: https://svn.code.sf.net/p/pspm/svn/trunk@447 23ec9670-3a1e-4a12-9187-a0ba8bce340d

--- pspm_glm_recon.m
+++ pspm_glm_recon.m
@@ -3,7 +3,7 @@ function [sts, glm] = pspm_glm_recon(modelfile)
 % Reconstructed responses are written into the field glm.resp, and 
 % reconstructed response peaks into the field glm.recon in original GLM file
 %
-% FORMAT: [sts, glm] = SCR_GLM_RECON(GLMFILE)
+% FORMAT: [sts, glm] = PSPM_GLM_RECON(GLMFILE)
 %
 %__________________________________________________________________________
 % PsPM 3.0
@@ -15,13 +15,13 @@ function [sts, glm] = pspm_glm_recon(modelfile)
 % initialise
 % -------------------------------------------------------------------------
 global settings;
-if isempty(settings), pspm_init; end;
+if isempty(settings), pspm_init; end
 sts = -1;
 
 % get GLM & basis functions
 % -------------------------------------------------------------------------
 [sts, glm] = pspm_load1(modelfile, 'all', 'glm');
-if sts ~= 1, return; end;
+if sts ~= 1, return; end
 bs = glm.bf.X;
 bfno = glm.bf.bfno;
 bfdur = size(bs, 1);
@@ -32,18 +32,18 @@ if strcmpi('ra_fc', glm.modelspec) && glm.bf.args == 1
     regdiff = 1;
 else
     regdiff = 0;
-end;
+end
 
 % find all non-nuisance regressors 
 % -------------------------------------------------------------------------
 n_nuis = sum(cellfun(@(c) ~isempty(c), regexpi(glm.names, '^R[0-9]*$', 'match')));
 regno = (numel(glm.names) - (glm.interceptno+n_nuis))/bfno;
-if regno ~= floor(regno), warning('Mismatch in basis functions and regressor number.'); return; end;
+if regno ~= floor(regno), warning('Mismatch in basis functions and regressor number.'); return; end
 
 % reconstruct responses
 % -------------------------------------------------------------------------
 resp = NaN(bfdur, regno);
-recon = cell(regno, 1);
+recon = NaN(regno, 1);
 condname = {};
 for k = 1:regno
     condname{k} = glm.names{((k - 1) * bfno + 1)};
@@ -51,12 +51,12 @@ for k = 1:regno
     condname{k} = [condname{k}(1:(foo-1)), ' recon']; clear foo;
     resp(:, k) = bs * glm.stats(((k - 1) * bfno + 1):(k * bfno));
     if regdiff
-        recon{k, 1} = diff(glm.stats(((k - 1) * bfno + 1):(k * bfno)));
+        recon(k, 1) = diff(glm.stats(((k - 1) * bfno + 1):(k * bfno)));
     else
         [~, bar] = max(abs(resp(:, k)));
-        recon{k, 1} = resp(bar, k);
-    end;
-end;
+        recon(k, 1) = resp(bar, k);
+    end
+end
 
 % save
 % -------------------------------------------------------------------------
