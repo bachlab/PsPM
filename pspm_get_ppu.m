0a1c94478063a46377356fdac56bdfbfdf1f9a8c
Tobias Moser <tmoser@mycompany.com>
2017-08-11 06:46:36 +0000

    enable import for ppu channels
    git-svn-id: https://svn.code.sf.net/p/pspm/svn/trunk@466 23ec9670-3a1e-4a12-9187-a0ba8bce340d

---
+++ pspm_get_ppu.m
@@ -0,0 +1,31 @@
+function [sts, data]=pspm_get_ppu(import)
+% SCR_GET_PPU is a common function for importing PPU data
+%
+% FORMAT:
+%   [sts, data]=pspm_get_ppu(import)
+%   with import.data: column vector of waveform data
+%        import.sr: sample rate
+%  
+%__________________________________________________________________________
+% PsPM 3.0
+% (C) 2015 Tobias Moser (University of Zurich)
+
+% $Id$
+% $Rev$
+
+global settings;
+if isempty(settings), pspm_init; end
+
+% initialise status
+sts = -1;
+
+% assign respiratory data
+data.data = import.data(:);
+
+% add header
+data.header.chantype = 'ppu';
+data.header.units = import.units;
+data.header.sr = import.sr;
+
+% check status
+sts = 1;
