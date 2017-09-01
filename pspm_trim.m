19041ceeed4b8a7c61de8a2ecfabb3d7626a4e05
Tobias Moser <tmoser@mycompany.com>
2017-09-01 07:57:53 +0000

    coding style changes + endmarker per file
    git-svn-id: https://svn.code.sf.net/p/pspm/svn/trunk@475 23ec9670-3a1e-4a12-9187-a0ba8bce340d

--- pspm_trim.m
+++ pspm_trim.m
@@ -47,29 +47,31 @@ elseif nargin<4
     warning('ID:invalid_input', 'No reference given.\n'); return;
 end
 
-if ~((ischar(from) && strcmpi(from, 'none')) || (isnumeric(from) && numel(from) == 1)) 
+if ~((ischar(from) && strcmpi(from, 'none')) ...
+        || (isnumeric(from) && numel(from) == 1)) 
     warning('ID:invalid_input', 'No valid start point given.\n'); return;
-elseif ~((ischar(to) && strcmpi(to, 'none')) || (isnumeric(to) && numel(to) == 1))
+elseif ~((ischar(to) && strcmpi(to, 'none')) ...
+        || (isnumeric(to) && numel(to) == 1))
     warning('ID:invalid_input', 'No end point given'); return;
 end
 
 if strcmpi(reference, 'marker')
     getmarker = 1;
     startmarker = 1;
-    endmarker = [];
+    g_endmarker = [];
 elseif isnumeric(reference) && numel(reference) == 2
     getmarker = 1;
     startmarker = reference(1);
-    endmarker = reference(2);    
-    reference = 'events';
+    g_endmarker = reference(2);    
     % check if reference markers are valid ---
-    if startmarker < 1 || endmarker < startmarker
+    if startmarker < 1 || g_endmarker < startmarker
         warning('ID:invalid_input', 'No valid reference markers.\n'); return;
     end
 elseif strcmpi(reference, 'file')
     getmarker = 0;
 else
-    warning('ID:invalid_input', 'Invalid reference option ''%s''', reference); return;
+    warning('ID:invalid_input', 'Invalid reference option ''%s''', reference); 
+    return;
 end
 
 % set options ---
@@ -79,7 +81,9 @@ catch
     options.overwrite = 0;
 end
 
-if ~isfield(options,'marker_chan_num') || ~isnumeric(options.marker_chan_num) || numel(options.marker_chan_num) > 1
+if ~isfield(options,'marker_chan_num') || ...
+        ~isnumeric(options.marker_chan_num) || ...
+        numel(options.marker_chan_num) > 1
     options.marker_chan_num = 0;
 end
 
@@ -98,6 +102,10 @@ clear datafile
 for d=1:numel(D)
     % determine file names ---
     datafile=D{d};
+
+    % set local endmarker back to global endmarker
+    % therefore endmarker is evaluated for each file individually
+    l_endmarker = g_endmarker;
         
     % user output ---
     if isstruct(datafile)
@@ -110,17 +118,19 @@ for d=1:numel(D)
     [sts, infos, data] = pspm_load_data(datafile, 0);
     if getmarker
         if options.marker_chan_num
-            [nsts, ninfos, ndata] = pspm_load_data(datafile, options.marker_chan_num);
+            [nsts, ~, ndata] = pspm_load_data(datafile, options.marker_chan_num);
             if ~strcmp(ndata{1}.header.chantype, 'marker')
-                warning('ID:invalid_option', 'Channel %i is no marker channel. The first marker channel in the file is used instead', options.marker_chan_num);
-                [nsts, ninfos, ndata] = pspm_load_data(datafile, 'marker');
+                warning('ID:invalid_option', ['Channel %i is no marker ', ...
+                    ' channel. The first marker channel in the file is ', ...
+                    'used instead'], options.marker_chan_num);
+                [nsts, ~, ndata] = pspm_load_data(datafile, 'marker');
             end
         else
-            [nsts, ninfos, ndata] = pspm_load_data(datafile, 'marker');
+            [nsts, ~, ndata] = pspm_load_data(datafile, 'marker');
         end
         sts = [sts; nsts];
         events = ndata{1}.data;
-        if isempty(endmarker), endmarker = numel(events); end
+        if isempty(l_endmarker), l_endmarker = numel(events); end
         clear nsts ninfos ndata
         
         if isempty(events)
@@ -146,12 +156,13 @@ for d=1:numel(D)
         endpoint = infos.duration;
     else
         if getmarker  % 'marker'
-            if endmarker > numel(events)
+            if l_endmarker > numel(events)
                 warning('ID:marker_out_of_range', ...
-                    '\nEnd marker (%03.0f) out of file - no trimming end end.\n', endmarker);
+                    ['\nEnd marker (%03.0f) out of file - no ', ...
+                   'trimming end end.\n'], g_endmarker);
                 endpoint = infos.duration;
             else
-                endpoint = events(endmarker) + to;
+                endpoint = events(l_endmarker) + to;
             end
         else          % 'file'
             endpoint = to;
@@ -160,11 +171,13 @@ for d=1:numel(D)
     
     % check start and end points ---
     if (startpoint < 0)
-        warning('ID:marker_out_of_range', '\nStart point (%.2f s) outside file, no trimming at start.', startpoint);
+        warning('ID:marker_out_of_range', ['\nStart point (%.2f s) outside', ...
+            ' file, no trimming at start.'], startpoint);
         startpoint = 0;
     end
     if endpoint > infos.duration
-        warning('ID:marker_out_of_range', '\nEnd point (%.2f s) outside file, no trimming at end.', endpoint);
+        warning('ID:marker_out_of_range', ['\nEnd point (%.2f s) outside ', ...
+            'file, no trimming at end.'], endpoint);
         endpoint = infos.duration;
     end
     
@@ -175,7 +188,8 @@ for d=1:numel(D)
             newstartpoint = floor(startpoint * data{k}.header.sr);
             if newstartpoint == 0, newstartpoint = 1; end
             newendpoint = floor(endpoint * data{k}.header.sr);
-            if newendpoint > numel(data{k}.data), newendpoint = numel(data{k}.data); end
+            if newendpoint > numel(data{k}.data), ...
+                newendpoint = numel(data{k}.data); end
             % trim data
             data{k}.data=data{k}.data(newstartpoint:newendpoint);
         else                                        % event channels
