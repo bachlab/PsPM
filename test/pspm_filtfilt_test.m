bf7857e736e1960a856416c1b3b9f453f17244ce
Ivan Rojkov <irojkov@student.ethz.ch>
2019-10-28 12:10:29 +0100

    Created a test for pspm_filtfilt

---
+++ test/pspm_filtfilt_test.m
@@ -0,0 +1,26 @@
+classdef pspm_filtfilt_test < matlab.unittest.TestCase 
+% unittest class for the pspm_hb2hp function
+%__________________________________________________________________________
+% PsPM TestEnvironment
+% (C) 2019 Ivan Rojkov (University of Zurich)
+
+    
+    methods (Test)
+        
+        function invalid_input(this)
+            
+            % Verify no input
+            this.verifyWarning(@() pspm_filtfilt(), 'ID:invalid_input');
+            
+            % Verify that 'a' isn't of dimension 1
+            this.verifyWarning(@() pspm_filtfilt(1,1,[1:10]), 'ID:invalid_input');
+            
+            % Verify that data must have length more than 3 times filter order.
+            this.verifyWarning(@() pspm_filtfilt([1:10],[1:20],[1:10]), 'ID:invalid_input');
+             
+        end
+        
+    end 
+    
+end
+
