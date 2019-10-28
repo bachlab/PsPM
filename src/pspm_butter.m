21e5fe991c56d2dbe6dda3c5d271aa145f5430e1
Ivan Rojkov <irojkov@student.ethz.ch>
2019-10-28 14:38:10 +0100

    Created a test for pspm_butter.m

--- src/pspm_butter.m
+++ src/pspm_butter.m
@@ -1,79 +1,80 @@
 function [sts, b, a] = pspm_butter(order, freqratio, pass)
-% This function interfaces Matlab Signal Processing Toolbox filters and
-% additionally implements a few standard filters for those who don''t have
-% this toolbox
-% FORMAT [sts, b, a] = pspm_butter(order, freqratio)
-% returns sts = -1 if non-standard filters are requested
-%
-%__________________________________________________________________________
-% PsPM 3.0
-% (c) 2009-2015 Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
-
-% $Id$ 
-% $Rev$
-
-% initialise
-% ------------------------------------------------------------------------
-global settings;
-if isempty(settings), pspm_init; end;
-sts = -1; a = []; b = [];
-errmsg = ' - please install the signal processing toolbox if you need other filters.';
-
-% check input arguments
-% ------------------------------------------------------------------------
-if nargin < 2
-    warning('Not enough input arguments.'); return;
-elseif nargin < 3
-    pass = 'low';
-elseif ~(any(strcmpi(pass, {'high', 'low'})))
-    warning('%s is not a valid argument.', pass); return;
-end;
+    % This function interfaces Matlab Signal Processing Toolbox filters and
+    % additionally implements a few standard filters for those who don''t have
+    % this toolbox
+    % FORMAT [sts, b, a] = pspm_butter(order, freqratio)
+    % returns sts = -1 if non-standard filters are requested
+    %
+    %__________________________________________________________________________
+    % PsPM 3.0
+    % (c) 2009-2015 Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
     
-if ~settings.signal && order ~= 1
-    warning('This function can only create 1st order filters - %s', errmsg); return; 
-end;
-
-% filters
-% ------------------------------------------------------------------------
-if settings.signal
-     [b, a]=butter(order, freqratio, pass);
-else
-    load('pspm_butter.mat');
-    switch pass
-        case 'low'
-            f = filt{1};
-        case 'high'
-            f = filt{2};
+    % $Id$ 
+    % $Rev$
+    
+    % initialise
+    % ------------------------------------------------------------------------
+    global settings;
+    if isempty(settings), pspm_init; end;
+    sts = -1; a = []; b = [];
+    errmsg = ' - please install the signal processing toolbox if you need other filters.';
+    
+    % check input arguments
+    % ------------------------------------------------------------------------
+    if nargin < 2
+        warning('ID:invalid_input','Not enough input arguments.'); return;
+    elseif nargin < 3
+        pass = 'low';
+    elseif ~(any(strcmpi(pass, {'high', 'low'})))
+        warning('ID:invalid_input','%s is not a valid argument.', pass); return;
     end;
-    d = abs([f.freqratio] - freqratio);
-    n = find(d < .0001);
-    if isempty(n)
-       warning('No filter implemented for this frequency ratio %s', errmsg); return;
+        
+    if ~settings.signal && order ~= 1
+        warning('ID:toolbox_missing','This function can only create 1st order filters - %s', errmsg); return; 
+    end;
+    
+    % filters
+    % ------------------------------------------------------------------------
+    if settings.signal
+         [b, a]=butter(order, freqratio, pass);
     else
-        if numel(n) > 1
-            [foo, n] = min(d);
+        load('pspm_butter.mat');
+        switch pass
+            case 'low'
+                f = filt{1};
+            case 'high'
+                f = filt{2};
         end;
-        a = f(n).a;
-        b = f(n).b;
+        d = abs([f.freqratio] - freqratio);
+        n = find(d < .0001);
+        if isempty(n)
+           warning('ID:toolbox_missing','No filter implemented for this frequency ratio %s', errmsg); return;
+        else
+            if numel(n) > 1
+                [foo, n] = min(d);
+            end;
+            a = f(n).a;
+            b = f(n).b;
+        end;
     end;
-end;
-
-sts = 1;
-return;
     
-% create filters (last used on 29.09.2013)
-% ------------------------------------------------------------------------
-% % lowpass
-% freqratio = [4.95/5 1./([2:4 5:5:500])];
-% for n = 1:numel(freqratio)
-%     [filt{1}(n).b filt{1}(n).a] = butter(1, freqratio(n));
-%     filt{1}(n).freqratio = freqratio(n);
-% end;
-% % highpass (standard filter DCM, standard filter GLM)
-% freqratio = [0.0159./([4.5 5:5:500]), 0.05./([4.5 5:5:500])];
-% for n = 1:numel(freqratio)
-%     [filt{2}(n).b filt{2}(n).a] = butter(1, freqratio(n), 'high');
-%     filt{2}(n).freqratio = freqratio(n);
-% end;
-% save([settings.path, 'pspm_butter.mat'], 'filt');
-
+    sts = 1;
+    return;
+        
+    % create filters (last used on 29.09.2013)
+    % ------------------------------------------------------------------------
+    % % lowpass
+    % freqratio = [4.95/5 1./([2:4 5:5:500])];
+    % for n = 1:numel(freqratio)
+    %     [filt{1}(n).b filt{1}(n).a] = butter(1, freqratio(n));
+    %     filt{1}(n).freqratio = freqratio(n);
+    % end;
+    % % highpass (standard filter DCM, standard filter GLM)
+    % freqratio = [0.0159./([4.5 5:5:500]), 0.05./([4.5 5:5:500])];
+    % for n = 1:numel(freqratio)
+    %     [filt{2}(n).b filt{2}(n).a] = butter(1, freqratio(n), 'high');
+    %     filt{2}(n).freqratio = freqratio(n);
+    % end;
+    % save([settings.path, 'pspm_butter.mat'], 'filt');
+    
+    
