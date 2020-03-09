1e210f3710545f38701f4b0c6142e10a03a5b93c
Ivan Rojkov <irojkov@student.ethz.ch>
2020-03-09 10:28:51 +0100

    Change basis functions follow-up discussion with Y

--- src/pspm_bf_spsrf_gamma.m
+++ src/pspm_bf_spsrf_gamma.m
@@ -1,7 +1,8 @@
-function [bs, t] = pspm_bf_spsrf_gamma( td,soa,p)
+function [bs, t] = pspm_bf_spsrf_gamma(varargin)
 % pspm_bf_spsrf_box basis 
 % 
-%   FORMAT: [bf p] = pspm_bf_spsrf_gamma(td, soa,p)
+%   FORMAT: [bf p] = pspm_bf_spsrf_gamma(td,soa,p) OR
+%           [bf p] = pspm_bf_spsrf_gamma([td,soa,p])
 %           with  td = time resolution in s
 %                 p(1) = A
 %                 p(2) = x0
@@ -15,20 +16,25 @@ function [bs, t] = pspm_bf_spsrf_gamma( td,soa,p)
 global settings
 if isempty(settings), pspm_init; end;
 
-
 %check input arguments
-if nargin < 1
-   errmsg='No sampling interval stated'; warning('ID:invalid_input',errmsg); return;
+if nargin==0
+    errmsg='No sampling interval stated'; warning('ID:invalid_input', errmsg); return;
+elseif nargin == 1
+    n_el = numel(varargin{1});
+    td = varargin{1}(1);
+    if n_el > 1, soa = varargin{1}(2); else , soa=3.5; end;
+elseif nargin > 1
+    td = varargin{1};
+    soa = varargin{2};
+    if nargin > 2 
+        p = varargin{3};
+        errmsg = 'Basis function parameter must be a numeric vector with 4 elements.'
+        if ~isnumeric(p) || numel(p)~=4, warning('ID:invalid_input', errmsg); return; end;
+    else
+        % parameters obtained by fitting a gamma function to smoothed test data
+        p = [-0.00953999201164847,-1.90202591900308,10.0912982464000,0.421253777432825];
+    end;
 end;
-
-if nargin < 2
-    soa = 3.5;
-end;
-
-if nargin < 3
-    p=[-0.007,0,4,0.5];
-end;
-
 
 A  = p(1);
 x0 = p(2);
