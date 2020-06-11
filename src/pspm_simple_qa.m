fb20443b161bea546cefcc45ac928e9f7ac6c1d7
Sam Maxwell <sam@paybase.io>
2020-06-11 12:04:46 +0100

    feat(epoch-file-out): function to create epochs and save from filter

--- src/pspm_simple_qa.m
+++ src/pspm_simple_qa.m
@@ -73,7 +73,33 @@ slope_filter(2:end) = abs(diff(data)*sr) < options.slope;
 filt = range_filter & slope_filter;
 d(filt) = data(filt);
 
+% write epochs to mat if missing_epochs_filename option is present
+if isfield(options, 'missing_epochs_filename')
+    epochs = collect_epochs(filt, sr)
+    save(options.missing_epochs_filename, 'epochs');
+end
+
 out = d;
 sts = 1;
 
+end
+
+% construct epochs using filt 0 as offset and 1 as onset
+function epochs = collect_epochs(filt, sr)
+    epochs = []
+    epoch_start = NaN
+    for i = 1:numel(filt)
+        if ~filt(i) & isnan(epoch_start)
+            epoch_start = (i - 1) / sr
+        end
+        if (filt(i) & ~isnan(epoch_start))
+            new_epoch = [ epoch_start, (i - 1) / sr ]
+            epochs = [ epochs; new_epoch ]
+            epoch_start = NaN
+        end
+    end
+    if ~isnan(epoch_start)
+        new_epoch = [ epoch_start, numel(filt) / sr ]
+        epochs = [ epochs; new_epoch ]
+    end
 end
