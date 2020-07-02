eed0493e1287ab07eb9a65f0c505e483b0af462f
Sam Maxwell <sam@paybase.io>
2020-07-02 19:14:11 +0100

    feat(scanpath-glm): update help texts

--- src/pspm_cfg/pspm_cfg_glm_sps.m
+++ src/pspm_cfg/pspm_cfg_glm_sps.m
@@ -30,7 +30,9 @@ glm_sps.prog = @pspm_cfg_run_glm_sps;
 soa         = cfg_entry;
 soa.name    = 'SOA';
 soa.tag     = 'soa';
-soa.help    = {['Specify custom SOA for response function. Tested values are 3.5s, 4s and 6s. Default: 3.5s']};
+soa.help    = {['Specify custom SOA for response function.', ...
+ 'Tested values are 3.5 s and 4 s.', ...
+ 'Default: 3.5 s']};
 soa.strtype = 'r';
 soa.num     = [1 1];
 soa.val     = {3.5};
@@ -39,18 +41,21 @@ soa.val     = {3.5};
 % SPS
 % bf = boxfunction
 spsrf_box = cfg_const;
-spsrf_box.name = 'Boxfunction';
+spsrf_box.name = 'Average scanpath speed';
 spsrf_box.tag = 'spsrf_box';
 spsrf_box.val = {'spsrf_box'};
-spsrf_box.help = {['SPSRF with boxfunction. (default)']};
+spsrf_box.help = {['This option implements a boxcar function over the SOA, and yields the average',...
+ 'scan path speed over that interval (i.e.', ...
+ 'the scan path length over that interval, divided by the SOA).', ...
+ '(default).']};
 
 % bf = gammafunction
 spsrf_gamma = cfg_const;
-spsrf_gamma.name = 'Gammafunction';
+spsrf_gamma.name = 'SPSRF_FC';
 spsrf_gamma.tag = 'spsrf_gamma';
 spsrf_gamma.val = {'spsrf_gamma'};
-spsrf_gamma.help = {['SPSRF with gammafunction.',...
-                   ' Use gamma probability density function to model the scanpath speed. ']};
+spsrf_gamma.help = {['This option implements a gamma function for fear-conditioned scan path speed', ...
+ 'responses, time-locked to the end of the CS-US interval.']};
 
 rf        = cfg_choice;
 rf.name   = 'Function';
