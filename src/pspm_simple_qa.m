8c3ccd450e9a11ad9438a72cab4c094918925e3a
Teddy <chaodadi@outlook.com>
2021-01-11 14:49:22 +0000

    Update pspm_simple_qa.m

--- src/pspm_simple_qa.m
+++ src/pspm_simple_qa.m
@@ -2,6 +2,7 @@ function [sts, out] = pspm_simple_qa(data, sr, options)
 	% pspm_simple_qa applies simple SCR quality assessment rulesets
 	% Rule 1:       Microsiemens values must be within range (0.05 to 60)
 	% Rule 2:       Absolute slope of value change must be less than 10 microsiemens per second
+	% V2
 	%
 	% FORMAT:
 	%	[sts, out] = pspm_simple_qa(data, sr, options)
