b7161d58f6525ee89e253f49927932f16a318ce9
Teddy <chaodadi@outlook.com>
2021-02-08 04:36:19 +0000

    Update pspm_simple_qa.m

--- src/pspm_simple_qa.m
+++ src/pspm_simple_qa.m
@@ -180,7 +180,7 @@ data_changed(filt) = data(filt);
 if isfield(options, 'missing_epochs_filename')
     if ~isempty(find(filt == 0, 1))
         epochs = filter_to_epochs(filt);
-        epochs_interval = downsample(epochs, sr);
+        %epochs_interval = downsample(epochs, sr);
         %save(options.missing_epochs_filename, 'epochs_interval');
     else
         % epochs = [];
