55bb75df494ca043effc9a97c663e7c3312dc6a6
Teddy <chaodadi@outlook.com>
2021-02-20 23:50:06 +0000

    Update pspm_load_data.m

--- src/pspm_load_data.m
+++ src/pspm_load_data.m
@@ -127,6 +127,7 @@ function [sts, infos, data, filestruct] = pspm_load_data(fn, chan)
         warning('ID:nonexistent_file', 'Data file (%s) doesn''t exist', fn); return;
     elseif exist(fn, 'file') && isstruct(chan) && ~chan.options.overwrite && ~chan.options.dont_ask_overwrite
         overwrite = menu(sprintf('File (%s) already exists. Overwrite?', fn), 'yes', 'no');
+		chan.options.overwrite = 1; % Set an initial value for Jenkins testing when no human user is there
         if overwrite == 1
             chan.options.overwrite = 1;
         else
