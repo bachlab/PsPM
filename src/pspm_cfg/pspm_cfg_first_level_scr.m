9a9b2c34594ecea5fffe871c35d9430fb310ab3c
Teddy <dadi.zhao@outlook.com>
2021-03-08 13:33:54 +0000

    Update scr_pp in GUI

--- src/pspm_cfg/pspm_cfg_first_level_scr.m
+++ src/pspm_cfg/pspm_cfg_first_level_scr.m
@@ -8,9 +8,6 @@ function cfg = pspm_cfg_first_level_scr
 cfg        = cfg_repeat;
 cfg.name   = 'SCR';
 cfg.tag    = 'scr';
-cfg.values = {pspm_cfg_glm_scr, ...
-				pspm_cfg_dcm, ...
-				pspm_cfg_sf, ...
-				pspm_cfg_pp_scr}; % Values in a cfg_repeat can be any cfg_item objects
+cfg.values = {pspm_cfg_glm_scr, pspm_cfg_dcm, pspm_cfg_sf}; % Values in a cfg_repeat can be any cfg_item objects
 cfg.forcestruct = true;
 cfg.help   = {''};
--- src/pspm_cfg/pspm_cfg_pp_scr.m
+++ src/pspm_cfg/pspm_cfg_pp_scr.m
@@ -1,4 +1,4 @@
-function pp_scr = pspm_cfg_pp_scr
+function [cfg] = pspm_cfg_pp_scr
 % function [cfg] = pspm_cfg_pp_scr
 %
 % Matlabbatch menu for simple skin conductance response (SCR) quality assessment
@@ -9,14 +9,15 @@ function pp_scr = pspm_cfg_pp_scr
 % $Id$
 % $Rev$
 
-% Initialise
-global settings
-if isempty(settings), pspm_init; end;
+%Data preprocessing
+cfg        = cfg_repeat;
+cfg.name   = 'SCR quality assessment';
+cfg.tag    = 'pp_scr';
+cfg.values = {pspm_cfg_pp_emg_data}; % To be changed
+cfg.forcestruct = true;
+cfg.help   = {'Help: SCR preprocessing'}; % To be changed
 
-% Data preprocessing
-%cfg        = cfg_repeat;
-%cfg.name   = 'SCR quality assessment';
-%cfg.tag    = 'pp_scr';
-%cfg.values = {pspm_cfg_pp_emg_data}; % To be changed
-%cfg.forcestruct = true;
-%cfg.help   = {'Help: SCR preprocessing'}; % To be changed
+% Initialise
+%global settings
+%if isempty(settings), pspm_init; end;
+
