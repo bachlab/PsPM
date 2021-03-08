ee4c3e51fa03a3e3e3c733491a24068f5dd3d172
Teddy <dadi.zhao@outlook.com>
2021-03-08 01:30:26 +0000

    Update pspm_split_sessions.m

--- src/pspm_split_sessions.m
+++ src/pspm_split_sessions.m
@@ -177,7 +177,8 @@ for d = 1:numel(D)
     
     if ~isempty(splitpoint)
         
-        %suffix = zeros(1,(numel(splitpoint)+1));% initialise
+        suffix = zeros(1,(numel(splitpoint)+1));% initialise
+        
         for s = 1:(numel(splitpoint)+1)
             if s == 1
                 sta = 1;
@@ -205,16 +206,15 @@ for d = 1:numel(D)
             %stop_time = mrk(sto)+mean_space;
             stop_time = mrk(sto);
             
-%             if options.suffix == 0
-%                 if sta == sto || options.randomITI
-%                     suffix(s) = mean(diff(mrk));
-%                 else
-%                     suffix(s) = mean(diff(mrk(sta:sto)));
-%                 end
-%             else
-%                 suffix(s) = options.suffix;
-%             end
-
+            if options.suffix == 0
+                if sta == sto || options.randomITI
+                    suffix(s) = mean(diff(mrk));
+                else
+                    suffix(s) = mean(diff(mrk(sta:sto)));
+                end
+            else
+                suffix(s) = options.suffix;
+            end
             
             % correct starttime (we cannot go into -) ---
             if start_time <= 0, start_time = 0; end
@@ -242,10 +242,9 @@ for d = 1:numel(D)
             
             if (splitpoint(sn,2) + options.suffix) > ininfos.duration
                 sto_p = ininfos.duration;
-                sto_suffix = ininfos.duration - splitpoint(sn,2);
+                suffix(sn) = ininfos.duration - splitpoint(sn,2);
             else
-                sto_p = splitpoint(sn,2) + options.suffix;
-                sto_suffix = options.suffix;
+                sto_p = splitpoint(sn,2) + suffix(sn);
             end
             
             % update infos ---
@@ -264,7 +263,7 @@ for d = 1:numel(D)
                 if strcmp(data{k}.header.units, 'events')
                     if k == markerchannel
                         startpoint = sta_p - sta_prefix;
-                        stoppoint = sto_p - sto_suffix;
+                        stoppoint = sto_p - suffix(sn);
                         foo = indata{k}.data;
                         foo_idx = find(foo<=stoppoint & foo>=startpoint);
                         foo(foo > stoppoint) = [];
