387a360032b16a637c780512be95a9c50f31651c
Teddy <dadi.zhao@outlook.com>
2021-04-19 22:38:47 +0100

    Update Help and Feedback

--- src/pspm_show_help_doc.m
+++ src/pspm_show_help_doc.m
@@ -2,16 +2,16 @@ function pspm_show_help_doc
 % PsPM 5.1
 % (C) 2008-2021 Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
 %
-% Updated by Teddy Chao (UCL)
+% Written by 2021 Teddy Chao
 
 % $Id$
 % $Rev$
 
-global settings;
-if isempty(settings)
-    pspm_init; 
-end
+% global settings;
+% if isempty(settings)
+%     pspm_init; 
+% end
 
-web('https://github.com/bachlab/PsPM/blob/develop/doc/PsPM_Manual.lyx', '-browser')
+web('http://pspm.sourceforge.net', '-browser')
 
 return
