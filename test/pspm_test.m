5c093261db5e2c1d1b502ab97c31fc81d697516f
Teddy <dadi.zhao@outlook.com>
2021-05-06 01:15:49 +0100

    Revert "Merge branch 'Issue-216' of https://github.com/bachlab/PsPM into Issue-216"
    This reverts commit 87cfc144399fed5ac7ec3eab99ab8392a420ba2c, reversing
    changes made to 2dded0e515875de773c4a101f7da4ef3c3b223b8.

--- test/pspm_test.m
+++ test/pspm_test.m
@@ -1,5 +1,5 @@
 function pspm_test(varargin)
-    %% pspm_test is a wrapper script for testing all testable functions in one
+    %% pspm_test is a wrapper script for testing all testable functions in one 
     % - to be used before any release.
     %
     % quit_after_tests : [bool]
@@ -10,7 +10,6 @@ function pspm_test(varargin)
     %__________________________________________________________________________
     % PsPM TestEnvironment
     % (C) 2013 Dominik Bach & Linus Ruettimann (University of Zurich)
-    % Updated 2021 Teddy Chao (WCHN, UCL)
 
     % imports
     % -------------------------------------------------------------------------
@@ -104,19 +103,19 @@ function pspm_test(varargin)
     [pth, fn, ext] = fileparts(which('pspm_test.m'));
     addpath(pth);
     pspm_init;
-    stats = run(full_suite);
+    stats = run(full_suite)
     n_failed = sum([stats.Failed]);
     success = n_failed == 0;
 
     if success
-        fprintf('pspm_test: All tests have passed!');
+        display('pspm_test: All tests have passed!');
     else
-        fprintf('pspm_test: Some tests have failed!');
+        display('pspm_test: Some tests have failed!');
     end
 
-    fprintf('===TEST_STATISTICS_BEGIN===');
+    display('===TEST_STATISTICS_BEGIN===');
     display(format_test_results(stats));
-    fprintf('===TEST_STATISTICS_END===');
+    display('===TEST_STATISTICS_END===');
     if quit_after_tests
         exit_code = 1 - success;
         quit(exit_code);
