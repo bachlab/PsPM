c534eb5319526d6b94b4bac52f9b546d5b6b3f4c
Teddy <dadi.zhao@outlook.com>
2021-06-06 03:58:03 +0100

    Update test function in split_sessions for only existing files

--- test/pspm_split_sessions_test.m
+++ test/pspm_split_sessions_test.m
@@ -105,9 +105,9 @@ classdef pspm_split_sessions_test < matlab.unittest.TestCase
             this.verifyEqual(numel(newdatafile), nsessions);
             
             for i = 1:numel(newdatafile)
-                [~, ~, d] = pspm_load_data(newdatafile{i});
-                this.verifyEqual(d{3}.data(1), 0);
                 if exist(newdatafile{i}, 'file')
+                [~, ~, d] = pspm_load_data(newdatafile{i});
+                    this.verifyEqual(d{3}.data(1), 0);
                     delete(newdatafile{i});
                 end
             end
