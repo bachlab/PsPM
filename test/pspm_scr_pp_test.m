3822e9532769f6579d268b2cfdb3fcad292d6fe7
Teddy <dadi.zhao@outlook.com>
2021-06-21 14:50:13 +0100

    Add channel testing

--- test/pspm_scr_pp_test.m
+++ test/pspm_scr_pp_test.m
@@ -4,12 +4,12 @@ classdef pspm_scr_pp_test < matlab.unittest.TestCase
     %__________________________________________________________________________
     % SCRalyze TestEnvironment
     % (C) 2013 Linus Rittimann (University of Zurich)
-    
+
     properties(Constant)
         fn = 'testdatafile79887.mat';
         duration = 10;
     end
-    
+
     methods (Test)
         function invalid_input(this)
             % test for invalid file
@@ -23,37 +23,38 @@ classdef pspm_scr_pp_test < matlab.unittest.TestCase
             % scr_pp is currently an indepedent function, so no need to
             % perform validation with other options like pspm_pp i think?
         end
-        
-        
+
+
         function scr_pp_test(this)
             %generate testdata
             channels{1}.chantype = 'scr';
-            
+
             %filter one channel
             options1 = struct('deflection_threshold', 0, ...
                 'expand_epochs', 0, ...
                 'channel_action', 'add');
-            
+
             options2 = struct('deflection_threshold', 0, ...
+                'channel', 'scr', ...
                 'expand_epochs', 0, ...
                 'channel_action', 'replace');
-            
-            options3 = struct('missing_epochs_filename', 'test_missing.mat', ...
-                'deflection_threshold', 0, ...
-                'expand_epochs', 0);
-            
+
+            options4 = struct('missing_epochs_filename', 'test_missing.mat', ...
+                    'deflection_threshold', 0, ...
+                    'expand_epochs', 0);
+
             pspm_testdata_gen(channels, this.duration, this.fn);
             [sts, ~, ~, filestruct] = pspm_load_data(this.fn, 'none');
             this.verifyTrue(sts == 1, 'the returned file couldn''t be loaded');
             this.verifyTrue(filestruct.numofchan == numel(channels), 'the returned file contains not as many channels as the inputfile');
-            
+
             % Verifying the situation without no missing epochs filename option and add the epochs to the file
             pspm_testdata_gen(channels, this.duration, this.fn);
             [~, out] = pspm_scr_pp(this.fn, options1);
             [sts_out, ~, ~, ~] = pspm_load_data(out{1}, 'none');
             % Verify out
             this.verifyTrue(sts_out == 1, 'the returned file couldn''t be loaded');
-            
+
             % Verifying the situation without no missing epochs filename option and replace the data in the file
             pspm_testdata_gen(channels, this.duration, this.fn);
             [~, out] = pspm_scr_pp(this.fn, options2);
@@ -61,7 +62,7 @@ classdef pspm_scr_pp_test < matlab.unittest.TestCase
             % Verify out
             this.verifyTrue(sts_out == 1, 'the returned file couldn''t be loaded');
             this.verifyTrue(filestruct_out.numofchan == numel(channels), 'the output has a different size');
-            
+
             % Verifying the situation with missing epochs filename option
             pspm_testdata_gen(channels, this.duration, this.fn);
             [~, out] = pspm_scr_pp(this.fn, options3);
@@ -70,10 +71,10 @@ classdef pspm_scr_pp_test < matlab.unittest.TestCase
             this.verifyTrue(sts_out == 1, 'the returned file couldn''t be loaded');
             this.verifyTrue(filestruct_out.numofchan == numel(channels), 'the output has a different size');
             % may need to verify epochs...
-            
+
             % test no file exists when not provided
             % this.verifyError(@()load('missing_epochs_test_out'), 'MATLAB:load:couldNotReadFile');
-            
+
             % Delete testdata
             delete(this.fn);
             delete('test_missing.mat');
