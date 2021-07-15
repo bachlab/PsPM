bbfc596c1506edf8fbafbca6120a163619fb74bd
Teddy <dadi.zhao@outlook.com>
2021-07-15 19:43:24 +0100

    restart jenkins for validate code

--- src/pspm_bf_data.m
+++ src/pspm_bf_data.m
@@ -2,12 +2,12 @@ function [bf, x] = pspm_bf_data(td)
 
 % pspm_bf_data provides a generic interface for creating a user-defined
 % response function from a data vector saved in a *.mat file. To use this
-% function, the variables 'datafile' and 'sr' need to be hard-coded. GLM 
+% function, the variables 'datafile' and 'sr' need to be hard-coded. GLM
 % can then be called with the optional argument model.bf = 'pspm_bf_data'.
 %
 % FORMAT:
-    % [bf, x]=pspm_bf_data(td) 
-% 
+    % [bf, x]=pspm_bf_data(td)
+%
 %_________________________________________________________________________
 % PsPM 5.1.1,
 % 2021 Dominik R Bach (University College London)
@@ -38,5 +38,3 @@ bf = [0; bf(:)];
 x  = [0; x(:)];
 
 return;
-
-
