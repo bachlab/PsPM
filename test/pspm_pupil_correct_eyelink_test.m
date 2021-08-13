bcb071d7adfa5fb98bbbef465d6de552e382b1a1
Teddy <dadi.zhao@outlook.com>
2021-08-13 22:04:50 +0100

    see comments
    1. correct_eyelink: reformat code
    2. correc_eyelink_test: added testfile restoring as some tests changed the channels

--- test/pspm_pupil_correct_eyelink_test.m
+++ test/pspm_pupil_correct_eyelink_test.m
@@ -1,12 +1,15 @@
 classdef pspm_pupil_correct_eyelink_test < pspm_testcase
+  
   % PSPM_PUPIL_CORRECT_EYELINK_TEST
   % unittest class for the pspm_pupil_correct_eyelink function
   %__________________________________________________________________________
   % PsPM TestEnvironment
   % (C) 2019 Eshref Yozdemir (University of Zurich)
+  % Updated 2021 Teddy Chao (WCHN, UCL)
   
   properties
-    raw_input_filename = fullfile('ImportTestData', 'eyelink', 'S114_s2.asc');
+    raw_input_filename = fullfile(...
+        'ImportTestData', 'eyelink', 'S114_s2.asc');
     pspm_input_filename = '';
   end
   
@@ -25,7 +28,8 @@ classdef pspm_pupil_correct_eyelink_test < pspm_testcase
       import{end + 1}.type = 'gaze_y_l';
       import{end + 1}.type = 'marker';
       options.overwrite = true;
-      this.pspm_input_filename = pspm_import(this.raw_input_filename, 'eyelink', import, options);
+      this.pspm_input_filename = pspm_import(this.raw_input_filename,...
+        'eyelink', import, options);
       this.pspm_input_filename = this.pspm_input_filename{1};
     end
   end
@@ -33,6 +37,7 @@ classdef pspm_pupil_correct_eyelink_test < pspm_testcase
   methods(Test)
     function invalid_input(this)
       opt = struct();
+      opt.channel_action = 'replace';
       
       this.verifyWarning(@()pspm_pupil_correct_eyelink(...
         52, opt), 'ID:invalid_input');
@@ -56,65 +61,88 @@ classdef pspm_pupil_correct_eyelink_test < pspm_testcase
         this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.C_z = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.C_z = 495;
-      this.verifyWarningFree(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt));
+      this.verifyWarningFree(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt));
+      backup(this); 
+      % verifyWarningFree changes data so restoring is required
       
       opt.screen_size_px = 'aoe';
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_px = 1;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_px = [1 2 3];
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_px = [-1920 1080];
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_px = [1920 1080];
       opt.screen_size_mm = 'aouet';
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_mm = 1;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_mm = [1 2 3];
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_mm = [-25 14];
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.screen_size_mm = [25 14];
-      this.verifyWarningFree(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt));
+      this.verifyWarningFree(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt));
       
       opt.mode = 'mixed';
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.mode = 'manual';
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.C_x = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.C_y = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.S_x = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.S_y = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.S_z = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:multiple_channels');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:multiple_channels');
+      % the previous "verifyWarningFree" introduced multiple channels
       
       opt.channel = 'gaze_x_l';
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
       
       opt.channel = 5;
-      this.verifyWarning(@()pspm_pupil_correct_eyelink(this.pspm_input_filename, opt), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, opt), 'ID:invalid_input');
     end
     
     function check_if_corrected_channel_is_saved(this)
@@ -124,13 +152,19 @@ classdef pspm_pupil_correct_eyelink_test < pspm_testcase
       options.C_z = 495;
       options.channel = 'pupil_l';
       
-      [sts, out_channel] = pspm_pupil_correct_eyelink(this.pspm_input_filename, options);
-      load(this.pspm_input_filename);
+      [~, out_channel] = pspm_pupil_correct_eyelink(...
+        this.pspm_input_filename, options);
+      testdata = load(this.pspm_input_filename);
       
-      this.verifyEqual(data{out_channel}.header.chantype, 'pupil_pp_l');
+      this.verifyEqual(...
+        testdata.data{out_channel}.header.chantype, 'pupil_pp_l');
       
-      ecg_chan_indices = find(cell2mat(cellfun(@(x) strcmp(x.header.chantype, 'pupil_l'), data, 'uni', false)));
-      this.verifyEqual(numel(data{ecg_chan_indices(end)}.data), numel(data{out_channel}.data));
+      ecg_chan_indices = find(...
+        cell2mat(cellfun(@(x) strcmp(x.header.chantype, 'pupil_l'),...
+         testdata.data, 'uni', false)));
+      this.verifyEqual(...
+        numel(testdata.data{ecg_chan_indices(end)}.data),...
+         numel(testdata.data{out_channel}.data));
     end
   end
   
