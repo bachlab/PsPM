35f1380efb0cf63dc0dff40d5174a318b8299c9d
Teddy <dadi.zhao@outlook.com>
2021-08-29 21:41:31 +0100

    Update test function for bf_spsrf_box

--- src/pspm_bf_spsrf_box.m
+++ src/pspm_bf_spsrf_box.m
@@ -1,16 +1,16 @@
 function [bs, x] = pspm_bf_spsrf_box(varargin)
 % pspm_bf_spsrf_box constructs a boxcar function to produce the averaged
-% scanpath speed over a 2-s time window in the end of SOA, which equals 
+% scanpath speed over a 2-s time window in the end of SOA, which equals
 % to scanpath length over a 2-s time window divided by 2/s
 %
-% FORMAT: [bs, x] = pspm_bf_spsrf_box(td, soa) 
+% FORMAT: [bs, x] = pspm_bf_spsrf_box(td, soa)
 %     OR: [bs, x] = pspm_bf_spsrf_box([td, soa])
 %
 % FORMAT: [bf p] = pspm_bf_spsrf_box(td, soa)
 %         with  td: time resolution in s
 %
 % REFERENCE
-% (1) Xia Y, Melinscak F, Bach DR (2020) 
+% (1) Xia Y, Melinscak F, Bach DR (2020)
 % Saccadic Scanpath Length: An Index for Human Threat Conditioning
 % Behavioral Research Methods (submitted)
 %________________________________________________________________________
@@ -18,19 +18,23 @@ function [bs, x] = pspm_bf_spsrf_box(varargin)
 
 % initialize
 global settings
-if isempty(settings), pspm_init; end;
+if isempty(settings), pspm_init; end
 
 % check input arguments
-if nargin==0
-    errmsg='No sampling interval stated'; warning('ID:invalid_input', errmsg); return;
+if nargin == 0
+  errmsg='No sampling interval stated'; warning('ID:invalid_input', errmsg); return;
 elseif nargin == 1
-    n_el = numel(varargin{1});
-    td = varargin{1}(1);
-    if n_el > 1, soa = varargin{1}(2); else , soa=3.5; end;
+  n_el = numel(varargin{1});
+  td = varargin{1}(1);
+  if n_el > 1
+    soa = varargin{1}(2); 
+  else
+    soa = settings.glm(10).filter.soa;
+  end
 elseif nargin > 1
-    td = varargin{1};
-    soa = varargin{2};
-end;
+  td = varargin{1};
+  soa = varargin{2};
+end
 
 
 % create border of interval
@@ -38,23 +42,23 @@ stop = soa;
 start = soa - 2;
 start_idx = floor(start/td)+1;
 if start_idx ==0
-    start_idx = start_idx+1;
-end 
-stop_idx = floor(stop/td); 
+  start_idx = start_idx+1;
+end
+stop_idx = floor(stop/td);
 
 if td > (stop-start)
-    warning('ID:invalid_input', 'Time resolution is larger than duration of the function.'); return;
+  warning('ID:invalid_input', 'Time resolution is larger than duration of the function.'); return;
 elseif td == 0
-    warning('ID:invalid_input', 'Time resolution must be larger than 0.'); return;
+  warning('ID:invalid_input', 'Time resolution must be larger than 0.'); return;
 elseif soa < 2
-    soa = 2;
-    stop = soa;
-    start = soa-2;
-    warning('Changing SOA to 2s to avoid implausible values (<2s).');
+  soa = 2;
+  stop = soa;
+  start = soa-2;
+  warning('Changing SOA to 2s to avoid implausible values (<2s).');
 elseif soa > 8
-    warning(['SOA longer than 8s is not recommended. ', ...
-        'Use at own risk.']);
-end;
+  warning(['SOA longer than 8s is not recommended. ', ...
+    'Use at own risk.']);
+end
 
 x = (0:td:stop-td)';
 bs = zeros(stop_idx,1);
--- test/pspm_bf_test.m
+++ test/pspm_bf_test.m
@@ -1,91 +1,93 @@
 classdef pspm_bf_test < matlab.unittest.TestCase
-% -------------------------------------------------------------------------
-% Testfunction to test the generic settings of a basis function.
-% The generic settings are:
-%
-% [y,x] = pspm_bf_<name>(td,<more args>)
-% [y,x] = pspm_bf_<name>([td,<more args>])
-%__________________________________________________________________________
-% PsPM TestEnvironment
-% (C) 2015 Tobias Moser (University of Zurich)
-
-% $Id: pspm_bf_test.m 467 2017-08-11 08:34:52Z tmoser $
-% $Rev: 467 $
-
-properties(Constant)
+  % -------------------------------------------------------------------------
+  % Testfunction to test the generic settings of a basis function.
+  % The generic settings are:
+  %
+  % [y,x] = pspm_bf_<name>(td,<more args>)
+  % [y,x] = pspm_bf_<name>([td,<more args>])
+  %__________________________________________________________________________
+  % PsPM TestEnvironment
+  % (C) 2015 Tobias Moser (University of Zurich)
+  % Update 2021 Teddy Chao (WCHN, UCL)
+  
+  properties(Constant)
     basis_function_prefix = 'pspm_bf_';
-end
-
-properties
+  end
+  
+  properties
     bf;
-end
-
-properties(MethodSetupParameter)
+  end
+  
+  properties(MethodSetupParameter)
     % Parameters for TestMethodSetup
-    basis_function = {'FIR', 'hprf', 'hprf_e', ... 
-        'hprf_fc', 'lcrf_gm', 'ldrf_gm', 'ldrf_gu', 'scrf', 'rarf_fc', ...
-        'psrf_fc', 'rarf_e', 'rarf_fc', 'rfrrf_e', 'rprf_e', 'sebrf',...
-        'spsrf_box'};
-end
-
-properties(TestParameter)
+    basis_function = {'FIR', 'hprf', 'hprf_e', ...
+      'hprf_fc', 'lcrf_gm', 'ldrf_gm', 'ldrf_gu', 'scrf', 'rarf_fc', ...
+      'psrf_fc', 'rarf_e', 'rarf_fc', 'rfrrf_e', 'rprf_e', 'sebrf',...
+      'spsrf_box'};
+  end
+  
+  properties(TestParameter)
     time_res_log = num2cell(-2:2);
-end
-
-methods(TestMethodSetup)
+  end
+  
+  methods(TestMethodSetup)
     function set_basis_function(this, basis_function)
-        fh_str = [this.basis_function_prefix, basis_function];
-        this.bf = str2func(fh_str);
+      fh_str = [this.basis_function_prefix, basis_function];
+      this.bf = str2func(fh_str);
     end
-end
-
-methods(Test)
+  end
+  
+  methods(Test)
     
     function invalid_input(this)
-        
-        % test with no parameters
-        this.verifyWarning(@() this.bf(), 'ID:invalid_input');
-        
-        % test with td > duration
-        % get duration
-        [output, t] = this.bf(0.1);
-        dur = numel(t)*0.1;       
-        % test with td > duration
-        td = dur + 1;
-        this.verifyWarning(@() this.bf(td), 'ID:invalid_input');
-        
-        % test with td = 0
-        this.verifyWarning(@() this.bf(0), 'ID:invalid_input');
-        
+      % test with no parameters
+      this.verifyWarning(@() this.bf(), 'ID:invalid_input');
+      % test with td > duration
+      % get duration
+      [~, t] = this.bf(0.1);
+      dur = numel(t)*0.1;
+      % test with td > duration
+      td = dur + 1;
+      this.verifyWarning(@() this.bf(td), 'ID:invalid_input');
+      % test with td = 0
+      this.verifyWarning(@() this.bf(0), 'ID:invalid_input');
     end
-        
+    
     function test_basic(this, time_res_log)
-        % test function for all basis functions
-        
-        % try to find out duration
+      % test function for all basis functions
+      
+      % try to find out duration
+      td = 0.1;
+      [~, t] = this.verifyWarningFree(@() this.bf(td));
+      dur = numel(t)*td;
+      
+      % calculate expected amount of returned elements with new td setting
+      td = 0.01;
+      n2 = dur/td;
+      
+      % call function and test if returned number of elements equals the
+      % expected number of elements
+      [~, t] = this.verifyWarningFree(@() this.bf(td));
+      this.verifyEqual(n2, numel(t));
+      
+      % test if the timepoints begin at either zero or at below zero -
+      % which then would cause a bfshift in glm
+      [~, t] = this.verifyWarningFree(@() this.bf(td));
+      this.verifyTrue(t(1) <= 0);
+      
+      % test time res log if dur > 10^time_res_log
+      td = 10^time_res_log;
+      if dur > td
+        [~, ~] = this.verifyWarningFree(@() this.bf(td));
+      end
+      
+      % test pspm_bf_spsrf_box
+      if isequal(this.bf,@pspm_bf_spsrf_box)
         td = 0.1;
-        [output, t] = this.verifyWarningFree(@() this.bf(td));
-        dur = numel(t)*td;
-           
-        % calculate expected amount of returned elements with new td setting
-        td = 0.01;
-        n2 = dur/td;
-
-        % call function and test if returned number of elements equals the
-        % expected number of elements
-        [output, t] = this.verifyWarningFree(@() this.bf(td));
-        this.verifyEqual(n2, numel(t));
-        
-        % test if the timepoints begin at either zero or at below zero -
-        % which then would cause a bfshift in glm
-        [output, t] = this.verifyWarningFree(@() this.bf([td]));
-        this.verifyTrue(t(1) <= 0);
-        
-        % test time res log if dur > 10^time_res_log
-        td = 10^time_res_log;
-        if dur > td
-            [output, t] = this.verifyWarningFree(@() this.bf(td));
-        end
+        this.verifyWarningFree(@() this.bf(td));
+        this.verifyWarningFree(@() this.bf([td,3.5]));
+        this.verifyWarningFree(@() this.bf(td,3.5));
+      end
     end
-end
+  end
 end
