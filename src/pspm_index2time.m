c1496951467ebb6506ae7c183b77cca4be7e18ac
Teddy <dadi.zhao@outlook.com>
2021-11-25 17:03:32 +0000

    Update help text

--- src/pspm_index2time.m
+++ src/pspm_index2time.m
@@ -1,25 +1,29 @@
 function time = pspm_index2time(index, sr, varargin)
 
-% pspm_index2time converts index to time.
+% DEFINITION
+%   pspm_index2time converts index to time.
 % FORMAT
 %   time = pspm_index2time(index, sr, varargin)
-% VARIABLES
-%   index           an integer
-%                   meaning: index / data point
-%   time            a decimal
-%                   meaning: time
-%   sr              a numerical value
-%                   meaning: sampling rate / frequency
-%   varargin
-%     precision   	an integer
-%                   meaning: how many digits to keep in the output
+% ARGUMENTS
+%   Input
+%     index        an integer
+%                  meaning: index / data point
+%     sr           a numerical value
+%                  meaning: sampling rate / frequency
+%   Output
+%     time         a decimal
+%                  meaning: time; unit: second
+%   Optional (varargin)
+%      precision   an integer
+%                  meaning: how many digits to keep in the output
 % PsPM 5.1.2
 % (C) 2021 Teddy Chao (WCHN, UCL)
+% Supervised by Professor Dominik Bach (WCHN, UCL)
 
 time = index / sr;
 if ~isempty(varargin)
-	precision = varargin{1};
-	time = round(time, precision)
+  precision = varargin{1};
+  time = round(time, precision)
 end
 
 end
--- src/pspm_time2index.m
+++ src/pspm_time2index.m
@@ -1,23 +1,27 @@
 function index = pspm_time2index(time, sr, varargin)
 
-% pspm_time2index converts time to index.
+% DEFINITION
+%   pspm_time2index converts time to index.
 % FORMAT
 %   index = pspm_time_conversion(time, sr, varargin)
-% VARIABLES
-%   time            a decimal
-%                   meaning: time
-%   index           an integer
-%                   meaning: index / data point
-%   sr              a numerical value
-%                   meaning: sampling rate / frequency
-%   varargin
-%     data_length   an integer
-%                   meaning: the length of data, by which data points should not exceed
+% ARGUMENTS
+%   Input
+%     time            a decimal
+%                     meaning: time; unit: second
+%     sr              a numerical value
+%                     meaning: sampling rate / frequency
+%     varargin
+%       data_length   an integer
+%                     meaning: the length of data, by which data points should not exceed
+%   Output
+%     index           an integer
+%                     meaning: index / data point
 % PsPM 5.1.2
 % (C) 2021 Teddy Chao (WCHN, UCL)
+% Supervised by Professor Dominik Bach (WCHN, UCL)
 
 if ~isempty(varargin)
-	data_length = varargin{1};
+  data_length = varargin{1};
 end
 index = round(time * sr);
 if index == 0
