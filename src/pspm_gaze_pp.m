6a9a2c4837472decbc852a472c3152d54d3a88be
Teddy <dadi.zhao@outlook.com>
2021-11-28 21:23:58 +0000

    Add gaze_pp

--- src/pspm_gaze_pp.m
+++ src/pspm_gaze_pp.m
@@ -10,7 +10,7 @@ function [sts, out_channel] = pspm_gaze_pp(fn, options)
 % VARIABLES
 % fn		[string] Path to the PsPM file which contains the gaze data.
 % options
-% 	channel	[numeric/string, optional] Channel ID to be preprocessed, default: 'gaze'.
+% 	channel	[numeric/string, optional] Channel ID to be preprocessed, default: 'gaze_x'.
 %
 % (C) 2021 Teddy Chao (WCHN, UCL)
 % Supervised by Professor Dominik Bach (WCHN, UCL)
@@ -24,105 +24,96 @@ sts = -1;
 
 %% 2 Create default arguments
 if nargin == 1
-	options = struct();
+  options = struct();
 end
 if ~isfield(options, 'channel')
-	options.channel = 'gaze';
+  options.channel = 'gaze_x';
 end
 if ~isfield(options, 'channel_action')
-	options.channel_action = 'add';
+  options.channel_action = 'add';
 end
 if ~isfield(options, 'channel_combine')
-	options.channel_combine = 'none';
+  options.channel_combine = 'none';
 end
 if ~isfield(options, 'plot_data')
-	options.plot_data = false;
+  options.plot_data = false;
 end
-	[lsts, default_settings] = pspm_gaze_pp_options();
-	if lsts ~= 1
-		return
-	end
-	if isfield(options, 'custom_settings')
-		default_settings = pspm_assign_fields_recursively(default_settings, options.custom_settings);
-	end
-	options.custom_settings = default_settings;
-	if ~isfield(options, 'segments')
-		options.segments = {};
-	end
+[lsts, default_settings] = pspm_pupil_pp_options();
+if lsts ~= 1
+  return
+end
+if isfield(options, 'custom_settings')
+  default_settings = pspm_assign_fields_recursively(default_settings, options.custom_settings);
+end
+options.custom_settings = default_settings;
+if ~isfield(options, 'segments')
+  options.segments = {};
+end
 
-	%% 3 Input checks
-	if ~ismember(options.channel_action, {'add', 'replace'})
-		warning('ID:invalid_input', 'Option channel_action must be either ''add'' or ''replace''');
-		return
-	end
-	for seg = options.segments
-		if ~isfield(seg{1}, 'start') || ~isfield(seg{1}, 'end') || ~isfield(seg{1}, 'name')
-			warning('ID:invalid_input', 'Each segment structure must have .start, .end and .name fields');
-			return
-		end
-	end
+%% 3 Input checks
+if ~ismember(options.channel_action, {'add', 'replace'})
+  warning('ID:invalid_input', 'Option channel_action must be either ''add'' or ''replace''');
+  return
+end
+for seg = options.segments
+  if ~isfield(seg{1}, 'start') || ~isfield(seg{1}, 'end') || ~isfield(seg{1}, 'name')
+    warning('ID:invalid_input', 'Each segment structure must have .start, .end and .name fields');
+    return
+  end
+end
 
-	%% 4 Load
-	action_combine = ~strcmp(options.channel_combine, 'none');
-	addpath(pspm_path('backroom'));
-	[lsts, data_x] = pspm_load_single_chan(fn, options.channel, 'last', 'gaze_x');
-	[lsts, data_y] = pspm_load_single_chan(fn, options.channel, 'last', 'gaze_y');
-	if lsts ~= 1
-		return
-	end
-	if action_combine
-		[lsts, data_combine_x] = pspm_load_single_chan(fn, options.channel_combine, 'last', 'gaze_x');
-		[lsts, data_combine_y] = pspm_load_single_chan(fn, options.channel_combine, 'last', 'gaze_y');
-		if lsts ~= 1
-			return
-		end
-		if strcmp(pspm_get_eye(data{1}.header.chantype), pspm_get_eye(data_combine{1}.header.chantype))
-			warning('ID:invalid_input', 'options.channel and options.channel_combine must specify different eyes');
-			return;
-		end
-		if data{1}.header.sr ~= data_combine{1}.header.sr
-			warning('ID:invalid_input', 'options.channel and options.channel_combine data have different sampling rate');
-			return;
-		end
-		if ~strcmp(data{1}.header.units, data_combine{1}.header.units)
-			warning('ID:invalid_input', 'options.channel and options.channel_combine data have different units');
-			return;
-		end
-		if numel(data{1}.data) ~= numel(data_combine{1}.data)
-			warning('ID:invalid_input', 'options.channel and options.channel_combine data have different lengths');
-			return;
-		end
-		old_chantype = sprintf('%s and %s', data{1}.header.chantype, data_combine{1}.header.chantype);
-	else
-		data_combine{1}.data = [];
-		old_chantype = data{1}.header.chantype;
-	end
-	rmpath(pspm_path('backroom'));
+%% 4 Load
+action_combine = ~strcmp(options.channel_combine, 'none');
+addpath(pspm_path('backroom'));
+[lsts, gaze_original] = pspm_load_single_chan(fn, options.channel, 'last', options.channel);
+if lsts ~= 1
+  return
+end
+if action_combine
+  [lsts, gaze_combine] = pspm_load_single_chan(fn, options.channel_combine, 'last', options.channel);
+  if lsts ~= 1
+    return
+  end
+  if strcmp(pspm_get_eye(gaze_original{1}.header.chantype), pspm_get_eye(gaze_combine{1}.header.chantype))
+    warning('ID:invalid_input', 'options.channel and options.channel_combine must specify different eyes');
+    return;
+  end
+  if gaze_original{1}.header.sr ~= gaze_combine{1}.header.sr
+    warning('ID:invalid_input', 'options.channel and options.channel_combine data have different sampling rate');
+    return;
+  end
+  if ~strcmp(gaze_original{1}.header.units, gaze_combine{1}.header.units)
+    warning('ID:invalid_input', 'options.channel and options.channel_combine data have different units');
+    return;
+  end
+  if numel(gaze_original{1}.data) ~= numel(gaze_combine{1}.data)
+    warning('ID:invalid_input', 'options.channel and options.channel_combine data have different lengths');
+    return;
+  end
+  old_chantype = sprintf('%s and %s', gaze_original{1}.header.chantype, gaze_combine{1}.header.chantype);
+else
+  gaze_combine{1}.data = [];
+  old_chantype = gaze_original{1}.header.chantype;
+end
+rmpath(pspm_path('backroom'));
 
-	%% 5 preprocess
-	[lsts, smooth_signal_x] = pspm_preprocess(data_x, data_combine_x, options.segments, options.custom_settings, options.plot_data, 'gaze_x');
-	[lsts, smooth_signal_y] = pspm_preprocess(data_y, data_combine_y, options.segments, options.custom_settings, options.plot_data, 'gaze_y');
-	if lsts ~= 1
-		return
-	end
+%% 5 preprocess
+[lsts, smooth_signal] = pspm_preprocess(gaze_original, gaze_combine, options.segments, options.custom_settings, options.plot_data, options.channel);
+if lsts ~= 1
+  return
+end
 
-	%% 6 save
-	channel_str = num2str(options.channel);
-	o.msg.prefix = sprintf(...
-	'Gaze X preprocessing :: Input channel: %s -- Input chantype: %s -- Output chantype: %s --', ...
-	channel_str, ...
-	old_chantype, ...
-	smooth_signal_x.header.chantype);
-	o.msg.prefix = sprintf(...
-	'Gaze Y preprocessing :: Input channel: %s -- Input chantype: %s -- Output chantype: %s --', ...
-	channel_str, ...
-	old_chantype, ...
-	smooth_signal_y.header.chantype);
-	[lsts, out_id] = pspm_write_channel(fn, smooth_signal_x, options.channel_action, o);
-	[lsts, out_id] = pspm_write_channel(fn, smooth_signal_y, options.channel_action, o);
-	if lsts ~= 1
-		return
-	end
-	out_channel = out_id.channel;
-	sts = 1;
+%% 6 save
+channel_str = num2str(options.channel);
+o.msg.prefix = sprintf(...
+  'Gaze preprocessing :: Input channel: %s -- Input chantype: %s -- Output chantype: %s --', ...
+  channel_str, ...
+  old_chantype, ...
+  smooth_signal.header.chantype);
+[lsts, out_id] = pspm_write_channel(fn, smooth_signal, options.channel_action, o);
+if lsts ~= 1
+  return
+end
+out_channel = out_id.channel;
+sts = 1;
 end
---
+++ test/pspm_gaze_pp_test.m
@@ -0,0 +1,116 @@
+classdef pspm_gaze_pp_test < pspm_testcase
+
+  % DEFINITION
+  % pspm_gaze_pp_test unittest classes for the pspm_gaze_pp function
+  % PsPM TestEnvironment
+  % (C) 2021 Teddy Chao (WCHN, UCL)
+  % Supervised by Professor Dominik Bach (WCHN, UCL)
+
+  properties
+    raw_input_filename = fullfile('ImportTestData', 'eyelink', 'S114_s2.asc');
+    pspm_input_filename = '';
+  end
+
+  methods(TestClassSetup)
+    function backup(this)
+      import = {};
+      import{end + 1}.type = 'pupil_r';
+      import{end}.eyelink_trackdist = 600;
+      import{end}.distance_unit = 'mm';
+      import{end + 1}.type = 'pupil_l';
+      import{end}.eyelink_trackdist = 600;
+      import{end}.distance_unit = 'mm';
+      import{end + 1}.type = 'gaze_x_r';
+      import{end + 1}.type = 'gaze_y_r';
+      import{end + 1}.type = 'gaze_x_l';
+      import{end + 1}.type = 'gaze_y_l';
+      import{end + 1}.type = 'marker';
+      options.overwrite = true;
+      this.pspm_input_filename = pspm_import(...
+        this.raw_input_filename, 'eyelink', import, options);
+      this.pspm_input_filename = this.pspm_input_filename{1};
+    end
+  end
+
+  methods(Test)
+    function invalid_input(this)
+      this.verifyWarning(@()pspm_gaze_pp(52), 'ID:invalid_input');
+      this.verifyWarning(@()pspm_gaze_pp('abc'), 'ID:nonexistent_file');
+
+      opt.channel = 'gaze_x_l';
+      this.verifyWarning(@()pspm_gaze_pp(this.pspm_input_filename, opt), 'ID:invalid_input');
+
+      opt.channel = 'gaze_x_l';
+      opt.channel_combine = 'pupil_l';
+      this.verifyWarning(@()pspm_gaze_pp(this.pspm_input_filename, opt), 'ID:invalid_input');
+
+      opt.channel_combine = 'gaze_y_l';
+      this.verifyWarning(@()pspm_gaze_pp(this.pspm_input_filename, opt), 'ID:invalid_input');
+    end
+
+    function check_if_preprocessed_channel_is_saved(this)
+      opt.channel = 'gaze_x_r';
+      [~, out_channel] = pspm_gaze_pp(this.pspm_input_filename, opt);
+      testdata = load(this.pspm_input_filename);
+      this.verifyEqual(testdata.data{out_channel}.header.chantype,'gaze_pp_x_r');
+      opt.channel = 'gaze_y_r';
+      [~, out_channel] = pspm_gaze_pp(this.pspm_input_filename, opt);
+      testdata = load(this.pspm_input_filename);
+      this.verifyEqual(testdata.data{out_channel}.header.chantype,'gaze_pp_y_r');
+    end
+
+    function check_upsampling_rate(this)
+      for freq = [500 1000 1500]
+        opt.custom_settings.valid.interp_upsamplingFreq = freq;
+        opt.channel = 'gaze_x_r';
+        [~, out_channel] = pspm_pupil_pp(this.pspm_input_filename, opt);
+        testdata = load(this.pspm_input_filename);
+        pupil_chan_indices = find(...
+          cell2mat(cellfun(@(x) strcmp(x.header.chantype, 'gaze_x_r'),...
+          testdata.data, 'uni', false)));
+        pupil_chan = pupil_chan_indices(end);
+        sr = testdata.data{pupil_chan}.header.sr;
+        upsampling_factor = freq / sr;
+
+        this.verifyEqual(...
+          numel(testdata.data{pupil_chan}.data) * upsampling_factor,...
+          numel(testdata.data{out_channel}.data));
+      end
+    end
+
+    function check_channel_combining(this)
+      opt.channel = 'gaze_x_r';
+      opt.channel_combine = 'gaze_x_l';
+      [~, out_channel] = pspm_gaze_pp(this.pspm_input_filename, opt);
+      testdata = load(this.pspm_input_filename);
+      this.verifyEqual(testdata.data{out_channel}.header.chantype, 'gaze_pp_x_c');
+      opt.channel = 'gaze_y_r';
+      opt.channel_combine = 'gaze_y_l';
+      [~, out_channel] = pspm_gaze_pp(this.pspm_input_filename, opt);
+      testdata = load(this.pspm_input_filename);
+      this.verifyEqual(testdata.data{out_channel}.header.chantype, 'gaze_pp_y_c');
+    end
+
+    function check_segments(this)
+      opt.channel = 'pupil_r';
+      opt.segments{1}.start = 5;
+      opt.segments{1}.end = 10;
+      opt.segments{1}.name = 'seg1';
+      opt.segments{2}.start = 25;
+      opt.segments{2}.end = 27;
+      opt.segments{2}.name = 'seg2';
+      [~, out_channel] = pspm_pupil_pp(this.pspm_input_filename, opt);
+      testdata = load(this.pspm_input_filename);
+
+      this.verifyTrue(isfield(testdata.data{out_channel}.header, 'segments'));
+      this.verifyEqual(testdata.data{out_channel}.header.segments{1}.name, 'seg1');
+      this.verifyEqual(testdata.data{out_channel}.header.segments{2}.name, 'seg2');
+    end
+  end
+
+  methods(TestClassTeardown)
+    function restore(this)
+      delete(this.pspm_input_filename);
+    end
+  end
+end
