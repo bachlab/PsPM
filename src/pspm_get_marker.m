68d47e6fb1cb0ba0118a42dd00335735627b1aa2
Teddy <dadi.zhao@outlook.com>
2022-01-24 03:24:09 +0000

    Update pspm_get_marker.m

--- src/pspm_get_marker.m
+++ src/pspm_get_marker.m
@@ -13,17 +13,17 @@ function [sts, data] = pspm_get_marker(import)
 %__________________________________________________________________________
 % PsPM 3.0
 % (C) 2008-2015 Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
+%     2022 Teddy Chao
 
 % $Id$
 % $Rev$
 
 
 global settings;
-if isempty(settings), pspm_init; end;
+if isempty(settings), pspm_init; end
 sts =-1;
 
 % get data
-% -------------------------------------------------------------------------
 [bsts, import] = pspm_get_events(import);
 if bsts~=1
     warning('ID:invalid_input','Call of pspm_get_events failed');
@@ -31,11 +31,14 @@ if bsts~=1
 end
 data.data = import.data;
 
-% add marker info
-% -------------------------------------------------------------------------
+% add flank info
+if isfield(import, 'flank')
+    data.flank = import.flank;
+end
+
 if isfield(import, 'markerinfo')
     data.markerinfo = import.markerinfo;
-end;
+end
 
 % add header
 % -------------------------------------------------------------------------
