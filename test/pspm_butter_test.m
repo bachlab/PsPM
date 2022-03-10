b58a71444c87cbcc411c34cf7a1cf1e0ec085659
Teddy <dadi.zhao@outlook.com>
2022-03-10 16:45:59 +0000

    Generalise hb2hp

--- test/pspm_butter_test.m
+++ test/pspm_butter_test.m
@@ -1,33 +1,33 @@
-classdef pspm_butter_test < matlab.unittest.TestCase 
-% unittest class for the pspm_hb2hp function
+classdef pspm_butter_test < matlab.unittest.TestCase
+% unittest class for the pspm_butter function
 %__________________________________________________________________________
 % PsPM TestEnvironment
 % (C) 2019 Ivan Rojkov (University of Zurich)
 
-    
+
     methods (Test)
-        
+
         function invalid_input(this)
-            
+
             global settings;
             if isempty(settings), pspm_init; end;
-            
-            settings.signal = 0; 
-            
+
+            settings.signal = 0;
+
             % Verify not enough input
             this.verifyWarning(@() pspm_butter(), 'ID:invalid_input');
-            
+
             % Verify that pass is either 'high' or 'low'
             this.verifyWarning(@() pspm_butter(1,1,'abc'), 'ID:invalid_input');
-            
+
             % Verify that Signal processing toolbox is missing #1
             this.verifyWarning(@() pspm_butter(2,1), 'ID:toolbox_missing');
-             
+
             % Verify that Signal processing toolbox is missing #2
             this.verifyWarning(@() pspm_butter(1,1), 'ID:toolbox_missing');
         end
-        
-    end 
-    
+
+    end
+
 end
 
--- test/pspm_filtfilt_test.m
+++ test/pspm_filtfilt_test.m
@@ -1,23 +1,23 @@
-classdef pspm_filtfilt_test < matlab.unittest.TestCase 
-% unittest class for the pspm_hb2hp function
+classdef pspm_filtfilt_test < matlab.unittest.TestCase
+% unittest class for the pspm_filtfilt function
 %__________________________________________________________________________
 % PsPM TestEnvironment
 % (C) 2019 Ivan Rojkov (University of Zurich)
 
-    
+
     methods (Test)
-        
+
         function invalid_input(this)
-            
+
             % Verify no input
             this.verifyWarning(@() pspm_filtfilt(), 'ID:invalid_input');
-            
+
             % Verify that data must have length more than 3 times filter order.
             this.verifyWarning(@() pspm_filtfilt([1:10],[1:20],[1:10]), 'ID:invalid_input');
-             
+
         end
-        
-    end 
-    
+
+    end
+
 end
 
