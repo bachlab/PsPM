7b99e42133690e85138722be979f59f2a179067d
Teddy <dadi.zhao@ucl.ac.uk>
2022-04-27 00:38:00 +0100

    Update pspm_convert_au2unit_test.m

--- test/pspm_convert_au2unit_test.m
+++ test/pspm_convert_au2unit_test.m
@@ -1,8 +1,6 @@
-classdef pspm_convert_ecg2hb_test < pspm_testcase
-
-    % pspm_convert_ecg2hb_test
-    % unittest class for the pspm_convert_au2unit function
-    %
-    % PsPM TestEnvironment
-
+classdef pspm_convert_au2unit_test < pspm_testcase
+  % ● Description
+  % unittest class for the pspm_convert_au2unit function
+  % ● Authorship
+  % (C) 2019 Eshref Yozdemir (University of Zurich)
 end
