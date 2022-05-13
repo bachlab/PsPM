ff0b6370a5a6ac109a2eedfb4825820c9bf7679f
Teddy <dadi.zhao@ucl.ac.uk>
2022-05-13 09:20:48 +0100

    sorting help text

--- src/pspm_bf_data.m
+++ src/pspm_bf_data.m
@@ -1,16 +1,17 @@
 function [bf, x] = pspm_bf_data(td)
-
+% Description
 % pspm_bf_data provides a generic interface for creating a user-defined
 % response function from a data vector saved in a *.mat file. To use this
 % function, the variables 'datafile' and 'sr' need to be hard-coded. GLM
 % can then be called with the optional argument model.bf = 'pspm_bf_data'.
 %
-% FORMAT:
-% 	[bf, x]=pspm_bf_data(td)
+% Format
+% [bf, x]=pspm_bf_data(td)
 %
+% Version
 % PsPM 5.1.1,
 % 2021 Dominik R Bach (University College London)
-% Updated 2021 Teddy Chao (WCHN, UCL)
+% 2021 Teddy Chao (UCL)
 
 %% Constants
 % CHANGE THIS TO CREATE YOUR FUNCTION
@@ -19,31 +20,24 @@ datafile = 'pspm_bf_data_sample.mat';
 % the data vector
 sr = 10;
 % this should be the sampling rate of your data vector, default to be 10
-
 %% Check input arguments
 if nargin==0
   errmsg='No sampling interval stated'; warning('ID:invalid_input', errmsg); return;
 end
-
 %% Load data
 if isempty(datafile)
   return
 end
 [~,~,indata,] = pspm_load_data(datafile, 1);
 data = indata{1,1}.data;
-
 %% Processing
 % determine original sampling points
 x_old = - 1/(2*sr) + (1:numel(data))/sr;
-
 % determine new sampling points
 x = (td:td:(numel(data)/sr));
-
 % resample
 bf = interp1(x_old, data, x, 'nearest', 'extrap');
-
 bf = [0; bf(:)];
 x  = [0; x(:)];
-
 return
 end
