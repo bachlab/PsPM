12479d379706797a3042403e74edc5280f1d0a78
Teddy <dadi.zhao@ucl.ac.uk>
2022-06-14 15:41:32 +0100

    convert nan to 0 in data.stats

--- src/pspm_con1.m
+++ src/pspm_con1.m
@@ -1,36 +1,32 @@
 function pspm_con1(modelfile, connames, convec, datatype, deletecon, options)
-% pspm_con1 creates contrasts on the first level (i.e. within one dataset)
-% and saves them to the modelfile to be accessed later
-%
-% FORMAT:
-% pspm_con1 (modelfile, connames, convec, [datatype, deletecon, options])
-%
-% modelfile: a filename, or cell array of filenames
-% connames: a cell array of names for the desired contrasts
-% convec: a cell array of contrasts
-%
-% optional arguments
-% datatype: 'param': use all parameter estimates
-%           'cond': GLM - contrasts formulated in terms of conditions,
-%                   automatically detects number of basis functions and
-%                   uses only the first one (i.e. without derivatives)
-%                   other models - contrasts based on unique trial names
-%           'recon': contrasts formulated in terms of conditions in a GLM,
-%                   reconstructs estimated response from all basis functions
-%                   and uses the peak of the estimated response
-%           deletecon: should existing contrasts be deleted (1) or
-%                   appended (0)?
-%                   default = 0;
-% options: options.zscored: 1 - zscore data
-%                               Restriction: only for non-linear models
-%                               and not when datatype == 'recon'
-%                           0 - do not zscore data
-%__________________________________________________________________________
-% PsPM 3.0
-% (C) 2008-2015 Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
-
-% $Id$
-% $Rev$
+% ● Description
+%   pspm_con1 creates contrasts on the first level (i.e. within one 
+%   dataset) and saves them to the modelfile to be accessed later.
+% ● Format
+%   pspm_con1 (modelfile, connames, convec, [datatype, deletecon, options])
+% ● Arguments
+%   modelfile:  a filename, or cell array of filenames.
+%    connames:  a cell array of names for the desired contrasts.
+%      convec:  a cell array of contrasts.
+%    datatype:  an optional structure
+%      .param:  use all parameter estimates
+%       .cond:  GLM - contrasts formulated in terms of conditions,
+%               automatically detects number of basis functions and uses 
+%               only the first one (i.e. without derivatives) other models 
+%               - contrasts based on unique trial names.
+%      .recon:  contrasts formulated in terms of conditions in a GLM,
+%               reconstructs estimated response from all basis functions
+%               and uses the peak of the estimated response
+%  .deletecon:  should existing contrasts be deleted (1) or appended (0)?
+%               default as 0;
+%     options: 
+%    .zscored: 1 - zscore data
+%                  Restriction: only for non-linear models
+%                  and not when datatype == 'recon'
+%              0 - do not zscore data
+% ● Version
+%   PsPM 3.0
+%   (C) 2008-2015 Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
 
 %% Initialise
 global settings
@@ -38,48 +34,46 @@ if isempty(settings)
   pspm_init;
 end
 sts = -1;
-
-% check input arguments
-% ------------------------------------------------------------------------
+%% 2 check input arguments
+% 2.1 check nargin
 if nargin<1
-  errmsg=sprintf('No modelfile specified'); warning(errmsg); return;
-elseif nargin<2
-  errmsg=sprintf('No contrast names specified'); warning(errmsg); return;
-elseif nargin<3
-  errmsg=sprintf('No contrasts specified'); warning(errmsg); return;
-elseif nargin<4
+  errmsg = 'No modelfile specified'; warning(errmsg); return;
+elseif nargin < 2
+  errmsg = 'No contrast names specified'; warning(errmsg); return;
+elseif nargin < 3
+  errmsg = 'No contrasts specified'; warning(errmsg); return;
+elseif nargin < 4
   datatype = 'param';
-end;
+end
 if nargin < 5
   deletecon = 0;
-end;
+end
 if nargin < 6
   options = struct();
-end;
-
-% check & convert filename --
+end
+% 2.2 check & convert filenames
 if ischar(modelfile)
   modelfile={modelfile};
 elseif ~iscell(modelfile)
   warning('Model file must be string or cell array of string.');
-end;
+end
 
 % check contrasts --
 if ~iscell(convec)
   warning('Please specify a cell array of contrast vectors.'); return;
-end;
+end
 for c=1:numel(convec)
   if ~isnumeric(convec{c})
     errmsg=sprintf('Contrast #%d is not a numeric vector.', c); warning(errmsg); return;
-  end;
-end;
+  end
+end
 
 % check contrast --
 if numel(connames)~=numel(convec)
   warning('Number of contrast names (%d) and number of contrast vectors (%d) don''t match.', ...
     numel(connames), numel(convec));
   return;
-end;
+end
 
 % store for output
 out_datatype = datatype;
@@ -90,26 +84,24 @@ switch datatype
   otherwise
     warning('Unknown datatype');
     return;
-end;
+end
 
 % set load1_options
 load1_options = struct('zscored',0);
 if isfield(options, 'zscored') && options.zscored
   load1_options.zscored = 1;
-end;
+end
 
-% work on contrasts
-% ------------------------------------------------------------------------
+%% 3 work on contrasts
 for iFn =1:numel(modelfile)
-  % user output --
+  % 3.1 user output --
   fprintf('Loading data ... ');
-
   % retrieve stats --
-  [sts, data, mdltype] = pspm_load1(modelfile{iFn}, datatype, '', load1_options);
+  [sts, data, ~] = pspm_load1(modelfile{iFn}, datatype, '', load1_options);
   if sts == -1
     warning('ID:invalid_input', 'Could not retrieve stats');
     return;
-  end;
+  end
   % create con structure or retrieve existing contrasts --
   if deletecon == 1
     con = []; conno = 0;
@@ -120,8 +112,8 @@ for iFn =1:numel(modelfile)
       con = []; conno = 0;
     else
       conno = numel(con);
-    end;
-  end;
+    end
+  end
 
   % user output --
   fprintf('\nWriting contrasts to %s\n', modelfile{iFn});
@@ -131,18 +123,20 @@ for iFn =1:numel(modelfile)
     if numel(convec{c}) > paramno
       warning('Contrast (%d) has more weights than statistics (%d) in modelfile %s', ...
         numel(convec{c}), paramno, modelfile{iFn}); return;
-    end;
-  end;
+    end
+  end
   % transform into row vector and right pad with zeros --
   conmat=zeros(numel(connames), paramno);
   for c=1:numel(convec)
     conmat(c,1:numel(convec{c}))=convec{c};
-  end;
+  end
 
   % calculate contrasts --
   % this automatically replicates contrasts across multiple measures if
   % data.stats has more than one column
-  conval = conmat * data.stats;
+  data_stats_converted = data.stats;
+  data_stats_converted(isnan(data.stats)) = 0;
+  conval = conmat * data_stats_converted;
 
   % zscored text-output for connames
   if isfield(data, 'zscored') && data.zscored
@@ -157,11 +151,11 @@ for iFn =1:numel(modelfile)
       for iMsr = 1:size(conval, 2)
         newconnames{iCon, iMsr} = sprintf('%s - %s%s', connames{iCon}, ...
           data.names{iMsr}, out_zscored);
-      end;
-    end;
+      end
+    end
   else
     newconnames = connames;
-  end;
+  end
 
   % save contrasts
   for iCon=1:numel(conval)
@@ -169,8 +163,8 @@ for iFn =1:numel(modelfile)
     con(conno+iCon).name   = newconnames{iCon};
     con(conno+iCon).con    = conval(iCon);
     indx = mod(iCon, size(conval, 1));
-    if indx == 0, indx = size(conval, 1); end;
+    if indx == 0, indx = size(conval, 1); end
     con(conno+iCon).convec = conmat(indx, :);
-  end;
+  end
   pspm_load1(modelfile{iFn}, 'savecon', con);
-end;
+end
