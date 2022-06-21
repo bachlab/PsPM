14b12e7adc7d16c6c9b8443069be2f5251bf0473
Teddy <dadi.zhao@ucl.ac.uk>
2022-06-21 15:32:41 +0100

    update pspm_pupil_pp
    put all subfunctions used by pupil_pp inside it

--- src/pspm_pupil_pp.m
+++ src/pspm_pupil_pp.m
@@ -383,3 +383,28 @@ for i = 1:numel(fnames)
 	end
 end
 end
+function eye = pspm_get_eye(chantype)
+% Definition
+% pspm_get_eye detect the eye location from an input channel type
+%	FORMAT
+%	eye = pspm_get_eye(chantype)
+% ARGUMENTS
+%   Input
+%     chantype  a string that contains the eye location
+%   Output
+%     eye    		a character
+% PsPM (version 5.1.2)
+% (C) 2021 Teddy Chao (UCL)
+eye = 'unknown';
+for eye_attempt = ['l', 'r', 'c']
+	if contains(chantype, ['_', eye_attempt, '_'])
+		eye = eye_attempt;
+	elseif chantype(length(chantype)-1:length(chantype)) == ['_', eye_attempt]
+		eye = eye_attempt;
+	end
+end
+if strcmp(eye, 'unknown')
+	warning('ID:invalid_input', 'chantype does not contain a valid eye');
+	return
+end
+end
