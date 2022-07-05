4ad7a9ee8178fab2911cb95c07fc49a3c3e0babf
Teddy <dadi.zhao@ucl.ac.uk>
2022-07-05 17:37:34 +0100

    Update pspm_con1.m

--- src/pspm_con1.m
+++ src/pspm_con1.m
@@ -130,22 +130,26 @@ for iFn = 1:numel(modelfile)
   % there are issues if data.stats has NaN and the corresponding conmat
   % also contains 0
   idx_issue = isnan(prod(data.stats,2));
-  idx_valid_issue = transpose(conmat==0) .* idx_issue;
-  idx_invalid_issue = transpose(conmat~=0) .* idx_issue;
+  l_conmat_r = length(conmat(:,1)); 
   if sum(idx_issue) > 0
-    if sum(idx_valid_issue) > 0 && sum(idx_invalid_issue) == 0
-      warning(['Calculated data.stats contain NaNs that are caused by unknown reasons. '...
-        'data.stats are then used in a contrasts, producing an invalid results (NaN).']);
-      data_stats_converted = data.stats;
-      data_stats_converted(isnan(data_stats_converted)) = 0;
-      conval = conmat * data_stats_converted;
-    else
-      warning(['Calculated data.stats contain NaNs that are caused by unknown reasons. '...
-        'However they were not used in the computation of the contrasts.']);
-      conval = conmat * data.stats;
+    for i_conmat_r = 1:l_conmat_r % if conmat is a 2D matrix, repeat for each row
+      conmat_array = conmat(i_conmat_r,:);
+      idx_valid_issue = transpose(conmat_array==0) .* idx_issue;
+      idx_invalid_issue = transpose(conmat_array~=0) .* idx_issue;
+      if sum(idx_valid_issue) > 0 && sum(idx_invalid_issue) == 0
+        warning(['Calculated data.stats contain NaNs that are caused by unknown reasons. '...
+          'data.stats are then used in a contrasts, producing an invalid results (NaN).']);
+        data_stats_converted = data.stats;
+        data_stats_converted(isnan(data_stats_converted)) = 0;
+        conval = conmat * data_stats_converted;
+      else
+        warning(['Calculated data.stats contain NaNs that are caused by unknown reasons. '...
+          'However they were not used in the computation of the contrasts.']);
+        conval = conmat * data.stats;
+      end
     end
   end
-    
+
   % zscored text-output for connames
   if isfield(data, 'zscored') && data.zscored
     out_zscored = ' (z-scored)';
