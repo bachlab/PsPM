ae380ad89a21df1347dd229cf633839c0f2c80b6
Teddy <dadi.zhao@ucl.ac.uk>
2022-07-12 18:36:43 +0100

    Update pspm_con1.m

--- src/pspm_con1.m
+++ src/pspm_con1.m
@@ -130,25 +130,25 @@ for iFn = 1:numel(modelfile)
   % there are issues if data.stats has NaN and the corresponding conmat
   % also contains 0
   idx_issue = any(isnan(data.stats),2);
-  l_conmat_r = length(conmat(:,1)); 
+  l_conmat_r = length(conmat(:,1));
+  data_stats_converted = data.stats;
+  conmat_converted = conmat;
   if any(idx_issue(:))
     for i_conmat_r = 1:l_conmat_r % if conmat is a 2D matrix, repeat for each row
       conmat_array = conmat(i_conmat_r,:);
       idx_valid_issue = transpose(conmat_array==0) .* idx_issue;
       idx_invalid_issue = transpose(conmat_array~=0) .* idx_issue;
-      if any(idx_valid_issue) && ~any(idx_invalid_issue)
+      if any(idx_valid_issue)
         warning(['Calculated data.stats contain NaNs that are caused by unknown reasons. '...
           'data.stats are then used in a contrasts, producing an invalid results (NaN).']);
-        data_stats_converted = data.stats;
-        data_stats_converted(isnan(data_stats_converted)) = 0;
-      else
+          data_stats_converted(idx_valid_issue==1,:) = 0;
+      elseif any(idx_invalid_issue)
         warning(['Calculated data.stats contain NaNs that are caused by unknown reasons. '...
           'However they were not used in the computation of the contrasts.']);
-        data_stats_converted = data.stats;
       end
     end
   end
-  conval = conmat * data_stats_converted;
+  conval = conmat_converted * data_stats_converted;
 
   % zscored text-output for connames
   if isfield(data, 'zscored') && data.zscored
