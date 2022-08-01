58225bec90fb606e93076cdff2e57107d67eceac
Teddy <dadi.zhao@ucl.ac.uk>
2022-08-01 18:32:31 +0100

    fix conflicts

--- src/pspm_filtfilt.m
+++ src/pspm_filtfilt.m
@@ -8,10 +8,10 @@ function y = pspm_filtfilt(b,a,x)
 %   a:  filter parameters (denominator)
 %   x:  input data vector (if matrix, filter over columns)
 %   y:  filtered data
-% ● Developer's Notes
+% ● Developer's notes
 %   The filter is described by the difference equation:
-%     y(n) = b(1)*x(n) + b(2)*x(n-1) + ... + b(nb+1)*x(n-nb)
-%                      - a(2)*y(n-1) - ... - a(na+1)*y(n-na)
+%   y(n) = b(1)*x(n) + b(2)*x(n-1) + ... + b(nb+1)*x(n-nb)
+%          - a(2)*y(n-1) - ... - a(na+1)*y(n-na)
 %   After filtering in the forward direction, the filtered sequence is then
 %   reversed and run back through the filter; Y is the time reverse of the
 %   output of the second filtering operation. The result has precisely zero
@@ -20,7 +20,7 @@ function y = pspm_filtfilt(b,a,x)
 %   transients by matching initial conditions.
 %   The length of the input x must be more than three times
 %   the filter order, defined as max(length(b)-1,length(a)-1).
-% ● References:
+% ● References
 %   [1] Sanjit K. Mitra, Digital Signal Processing, 2nd ed, McGraw-Hill, 2001
 %   [2] Fredrik Gustafsson, Determining the initial states in forward-backward
 %       filtering, IEEE Transactions on Signal Processing, pp. 988--992,
