98cb502246cae7654ffe9906de72c93389ecdf49
teddy.chao <dadi.zhao@ucl.ac.uk>
2022-10-06 18:35:24 +0100

    verify option subfields

--- src/pspm_options.m
+++ src/pspm_options.m
@@ -13,61 +13,61 @@ global settings
 if isempty(settings)
   pspm_init;
 end
-StrOptChanInvalid = 'options.channel must contain valid channel types or positive integers.';
-StrOptChanInvalidChar = 'options.channel is not a valid channel type.';
+text_optional_channel_invalid = 'options.channel must contain valid channel types or positive integers.';
+text_optional_channel_invalid_char = 'options.channel is not a valid channel type.';
 switch FunName
   case 'blink_saccade_filt'
-    options = Autofill(options,'channel', 0);
-    options = AutofillChanAction(options);
+    options = autofill(options, 'channel', 0);
+    options = autofill_channel_action(options);
   case 'compute_visual_angle_core'
-    options = Autofill(options,'interpolate',0);
+    options = autofill(options,'interpolate',0);
   case 'compute_visual_angle'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'con1'
-    options = AutofillChanAction(options,'zscored',0);
+    options = autofill_channel_action(options,'zscored',0);
   case 'convert_area2diameter'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'convert_au2unit'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'convert_ecg2hb_amri'
-    options = Autofill(options, 'channel', 'ecg');
-    options = AutofillChanAction(options, 'replace');
+    options = autofill(options, 'channel', 'ecg');
+    options = autofill_channel_action(options, 'replace');
   case 'convert_gaze_distance'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'convert_hb2hp'
-    options = AutofillChanAction(options, 'replace');
+    options = autofill_channel_action(options, 'replace');
   case 'convert_pixel2unit'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'convert_ppg2hb'
-    options = Autofill(options, 'channel', 'ppg2hb');
-    options = AutofillChanAction(options, 'replace');
+    options = autofill(options, 'channel', 'ppg2hb');
+    options = autofill_channel_action(options, 'replace');
   case 'convert_visangle2sps'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'emg_pp'
-    options = Autofill(options, 'channel', 'emg');
-    options = AutofillChanAction(options, 'replace');
+    options = autofill(options, 'channel', 'emg');
+    options = autofill_channel_action(options, 'replace');
   case 'exp'
-    options = Autofill(options, 'target', 'screen');
-    options = Autofill(options, 'statstype', 'param');
-    options = Autofill(options, 'delim', '\t');
-    options = Autofill(options, 'exclude_missing', 0);
+    options = autofill(options, 'target', 'screen');
+    options = autofill(options, 'statstype', 'param');
+    options = autofill(options, 'delim', '\t');
+    options = autofill(options, 'exclude_missing', 0);
   case 'find_sound'
-    options = AutofillChanAction(options, 'none', {'add','replace','none'});
+    options = autofill_channel_action(options, 'none', {'add','replace','none'});
   case 'find_valid_fixations'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
   case 'gaze_pp'
-    options = Autofill(options, 'channel', 'gaze_x_l');
-    options = AutofillChanAction(options, 'add', {'add','replace','none'});
-    options = Autofill(options, 'channel_combine', 'none');
-    options = Autofill(options, 'segments', {});
-    options = Autofill(options, 'valid_sample', 0);
-    options = Autofill(options, 'plot_data', false);
+    options = autofill(options, 'channel', 'gaze_x_l');
+    options = autofill_channel_action(options, 'add', {'add','replace','none'});
+    options = autofill(options, 'channel_combine', 'none');
+    options = autofill(options, 'segments', {});
+    options = autofill(options, 'valid_sample', 0);
+    options = autofill(options, 'plot_data', false);
   case 'glm'
-    options = Autofill(options, 'modelspec', 'scr');
-    options = Autofill(options, 'bf', 0);
-    options = Autofill(options, 'overwrite', 0, 1);
-    options = Autofill(options, 'norm', 0);
-    options = Autofill(options, 'centering', 1);
+    options = autofill(options, 'modelspec', 'scr');
+    options = autofill(options, 'bf', 0);
+    options = autofill(options, 'overwrite', 0, 1);
+    options = autofill(options, 'norm', 0);
+    options = autofill(options, 'centering', 1);
     if ~isfield(options, 'marker_chan_num')
       options.marker_chan_num = 'marker';
     elseif ~(isnumeric(options.marker_chan_num) && numel(options.marker_chan_num)==1)
@@ -84,34 +84,34 @@ switch FunName
       end
     end
   case 'load1'
-    options = Autofill(options, 'overwrite', 0, 1);
-    options = Autofill(options, 'zscored', 0);
+    options = autofill(options, 'overwrite', 0, 1);
+    options = autofill(options, 'zscored', 0);
   case 'import'
-    % options = Autofill(options, 'overwrite', 0, 1);
+    % options = autofill(options, 'overwrite', 0, 1);
   case 'interpolate'
-    options = AutofillChanAction(options);
+    options = autofill_channel_action(options);
     try options.overwrite; catch, options.overwrite = 0; end
   case 'sf'
-    options = Autofill(options,'overwrite', 0);
+    options = autofill(options,'overwrite', 0);
     if ~isfield(options,'marker_chan_num') ||...
   			~isnumeric(options.marker_chan_num) ||...
   			numel(options.marker_chan_num) > 1
       options.marker_chan_num = 0;
     end
   case 'split_sessions'
-    options = Autofill(options, 'overwrite', 0, 1);
-    options = Autofill(options, 'prefix', 0);
-    options = Autofill(options, 'suffix', 0);
-    options = Autofill(options, 'verbose', 0);
-    options = Autofill(options, 'splitpoints', []);
-    options = Autofill(options, 'missing', 0);
-    options = Autofill(options, 'randomITI', 0);
-    options = Autofill(options, 'max_sn', settings.split.max_sn);
+    options = autofill(options, 'overwrite', 0, 1);
+    options = autofill(options, 'prefix', 0);
+    options = autofill(options, 'suffix', 0);
+    options = autofill(options, 'verbose', 0);
+    options = autofill(options, 'splitpoints', []);
+    options = autofill(options, 'missing', 0);
+    options = autofill(options, 'randomITI', 0);
+    options = autofill(options, 'max_sn', settings.split.max_sn);
     % maximum number of sessions (default 10)
-    options = Autofill(options, 'min_break_ratio', settings.split.min_break_ratio);
+    options = autofill(options, 'min_break_ratio', settings.split.min_break_ratio);
     % minimum ratio of session break to normal inter marker interval (default 3)
   case 'trim'
-    options = Autofill(options, 'overwrite', 0, 1);
+    options = autofill(options, 'overwrite', 0, 1);
     if ~isfield(options,'marker_chan_num') || ...
   			~isnumeric(options.marker_chan_num) || ...
   			numel(options.marker_chan_num) > 1
@@ -123,71 +123,71 @@ switch FunName
     end
   case 'write_channel'
     if ~isfield(options, 'channel')
-      warning('ID:invalid_input', StrOptChanInvalid);
+      warning('ID:invalid_input', text_optional_channel_invalid);
       return
     else
       switch class(options.channel)
   			case 'char'
   				if ~any(strcmpi(options.channel,{'add','replace','none'}))
-  					warning('ID:invalid_input', StrOptChanInvalidChar);
+  					warning('ID:invalid_input', text_optional_channel_invalid_char);
   					return
   				end
   			case 'double'
   				if (any(mod(options.channel, 1)) || any(options.channel<0))
-  					warning('ID:invalid_input', StrOptChanInvalid);
+  					warning('ID:invalid_input', text_optional_channel_invalid);
   					return
   				end
   			otherwise
-  				warning('ID:invalid_input', StrOptChanInvalid);
+  				warning('ID:invalid_input', text_optional_channel_invalid);
   				return
       end
     end
 end
 end
 
-function options = Autofill(varargin)
+function options = autofill(varargin)
 switch nargin
   case 3
     options = varargin{1};
-    FieldName = varargin{2};
-    DefaultValue = varargin{3};
-    if ~isfield(options, FieldName)
-      options.(FieldName) = DefaultValue;
+    field_name = varargin{2};
+    default_value = varargin{3};
+    if ~isfield(options, field_name)
+      options.(field_name) = default_value;
     end
   case 4
     options = varargin{1};
-    FieldName = varargin{2};
-    DefaultValue = varargin{3};
-    AcceptableValue = varargin{4};
-    if ~isfield(options, FieldName)
-      options.(FieldName) = DefaultValue;
+    field_name = varargin{2};
+    default_value = varargin{3};
+    acceptable_value = varargin{4};
+    if ~isfield(options, field_name)
+      options.(field_name) = default_value;
     else
-      if options.(FieldName) ~= AcceptableValue && options.(FieldName) ~= DefaultValue
-        options.(FieldName) = DefaultValue;
+      if options.(field_name) ~= acceptable_value && options.(field_name) ~= default_value
+        options.(field_name) = default_value;
       end
     end
   otherwise
-    warning('ID:invalid_input', 'Autofill needs at least 3 arguments');
+    warning('ID:invalid_input', 'autofill needs at least 3 arguments');
     return
 end
 end
 
-function options = AutofillChanAction(options, varargin)
+function options = autofill_channel_action(options, varargin)
 switch nargin
   case 1
-    DefaultValue = 'add';
-    OptValue = {'add', 'replace'};
+    default_value = 'add';
+    optional_value = {'add', 'replace'};
   case 2
-    DefaultValue = varargin{1};
-    OptValue = {'add', 'replace'};
+    default_value = varargin{1};
+    optional_value = {'add', 'replace'};
   case 3
-    DefaultValue = varargin{1};
-    OptValue = varargin{2};
+    default_value = varargin{1};
+    optional_value = varargin{2};
 end
 if ~isfield(options, 'channel_action')
-  options.channel_action = DefaultValue;
+  options.channel_action = default_value;
 else
-  if ~any(strcmpi(options.channel_action, OptValue))
+  if ~any(strcmpi(options.channel_action, optional_value))
     warning('ID:invalid_input', ...
       '''options.channel_action'' must be among accepted values.');
     return
