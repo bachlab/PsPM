f84b62f30affe4d78c0e82da3ac59849cb8db174
Teddy <dadi.zhao@ucl.ac.uk>
2023-01-10 15:03:03 +0000

    set -1 as default
    when .length is -1, further operations will be required. .length=-1 is permitted in pspm_extract_segments

--- src/pspm_options.m
+++ src/pspm_options.m
@@ -177,7 +177,7 @@ switch FunName
     options = autofill(options, 'target',                 'screen',   '*Char'           );
   case 'extract_segments'
     %% 2.22 pspm_extract_segments
-    options = autofill(options, 'length',                 1,          '>', 0            );
+    options = autofill(options, 'length',                 -1,         '>', 0            );
     options = autofill(options, 'norm',                   0,          1                 );
     options = autofill(options, 'outputfile',             '',         '*Char'           );
     options = autofill(options, 'overwrite',              0,          [1, 2]            );
