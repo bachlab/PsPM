86f89ef00fe2a6623b6b5c11fde74fb4a7a8db61
dadi.zhao <dadi.zhao@outlook.com>
2023-02-21 10:33:58 +0000

    remove interpolation

--- src/pspm_compute_visual_angle_core.m
+++ src/pspm_compute_visual_angle_core.m
@@ -1,23 +1,23 @@
 function [lat, lon, lat_range, lon_range] = pspm_compute_visual_angle_core(x_data, y_data, width, height, distance, options)
-% ● Description
+% ? Description
 %   pspm_compute_visual_angle computes from gaze data the corresponding
 %   visual angle (for each data point). The convention used here is that the
 %   origin of coordinate system for gaze data is at the bottom left corner of
 %   the screen.
-% ● Format
+% ? Format
 %   [lat, lon, lat_range, lon_range] = pspm_compute_visual_angle_core(x_data, y_data, options)
-% ● Arguments
+% ? Arguments
 %           x_data: X axis data
 %           y_data: y axis data
 %            width: screen width in same units as data
 %           height: screen height in same units as data
 %         distance: screen distance in same units as data
-% ● Output
+% ? Output
 %              lat: the latitude in degrees
 %              lon: the longitude in degrees
 %        lat_range: the latitude range
 %        lon_range: the longitude range
-% ● History
+% ? History
 %   Introduced in PsPM 4.0
 
 %% 1 Initialise
@@ -30,33 +30,25 @@ options = pspm_options(options, 'compute_visual_angle_core');
 if options.invalid
   return
 end
-if options.interpolate
-  interpolate_options = struct('extrapolate', 1);
-  [ sts_x, gx_d ] = pspm_interpolate(x_data, interpolate_options);
-  [ sts_x, gy_d ] = pspm_interpolate(y_data, interpolate_options);
-else
-  gx_d = x_data;
-  gy_d = x_data;
-end
 % The convention is that the origin of the screen is in the bottom
 % left corner, so the following line is not needed a priori, but I
 % leave it anyway just in case :
 % gy_d = data{gy}.header.range(2)-gy_d;
-N = numel(gx_d);
-if N ~= numel(gy_d)
+N = numel(x_data);
+if N ~= numel(y_data)
   warning('ID:invalid_input', 'length of data in gaze_x and gaze_y is not the same');
   return;
-end;
+end
 %% 2 move (0,0) into center of the screen
-gx_d = gx_d - width/2;
-gy_d = gy_d - height/2;
+x_data = x_data - width/2;
+y_data = y_data - height/2;
 %% 3 compute visual angle for gaze_x and gaze_y data:
 % 1) x axis in cartesian coordinates
-s_x = gx_d;
+s_x = x_data;
 % 2) y axis in cartesian coordinates, actually the distance from participant to the screen
-s_y = distance * ones(numel(gx_d),1);
+s_y = distance * ones(numel(x_data),1);
 % 3) z axis in spherical coordinates, actually the y axis of the screen
-s_z = gy_d;
+s_z = y_data;
 % 4) convert cartesian to spherical coordinates in radians,
 %    where azimuth = longitude, elevation = latitude
 %    the center of spherical coordinates are the eyes of the subject
