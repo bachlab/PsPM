cf52ad4dd7777ab4c21ef85e775145496516e090
dadi.zhao <dadi.zhao@outlook.com>
2023-05-20 05:07:22 +0100

    Update pspm.m

--- src/pspm.m
+++ src/pspm.m
@@ -1,9 +1,9 @@
 function pspm(varargin)
-% œ Description
+% â— Description
 %   pspm.m handles the main GUI for PsPM
-% œ Last Updated in
+% â— Last Updated in
 %   PsPM 6.1
-% œ History
+% â— History
 %   Written in 13-09-2022 by Teddy Chao (UCL)
 
 if verLessThan('matlab','9.4')
