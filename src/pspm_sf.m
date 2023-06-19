1ef68b23eed693bac8911f89104b7e5a1535f8ba
teddy.chao <dadi.zhao@ucl.ac.uk>
2023-06-19 12:50:17 +0100

    update sf

--- src/pspm_sf.m
+++ src/pspm_sf.m
@@ -225,7 +225,31 @@ for iFile = 1:numel(model.datafile)
         return;
       end
     end
-    events = data{1}.data;
+    switch class(options.marker_chan_num_event)
+      case 'double'
+        if options.marker_chan_num_event>length(data)
+          warning('ID:invalid_input', ...
+            'options.marker_chan_num_event exceeds the length of data');
+          return
+        else
+          events = data{options.marker_chan_num_event}.data(:);
+        end
+      case 'char'
+        if length(data) == 1
+          events{iFile} = data.data(:);
+        else
+          if strcmp(options.marker_chan_num_event, 'first')
+            events{iFile} = data{1}.data(:);
+          elseif strcmp(options.marker_chan_num_event, 'last')
+            events{iFile} = data{end}.data(:);
+          else
+            warning('ID:invalid_input', ...
+              'options.marker_chan_num_event can only specify first or last channel as a char.');
+            return
+          end
+        end
+      end
+    end
   end
   for iEpoch = 1:size(epochs{iFile}, 1)
     if iEpoch > 1, fprintf('\n\t\t\t'); end
