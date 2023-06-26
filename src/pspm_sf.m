ef5cdaf5e43bd935a512e84eae8499e5bdd89584
teddy.chao <dadi.zhao@ucl.ac.uk>
2023-06-26 15:18:50 +0100

    Update pspm_sf.m

--- src/pspm_sf.m
+++ src/pspm_sf.m
@@ -186,16 +186,14 @@ for iFile = 1:nFile
   % 3.3 get and filter data
   [sts_load_data, ~, data] = pspm_load_data(model.datafile{iFile}, model.channel);
   if sts_load_data < 1, return; end
-  y{1} = data{1}.data;
-  sr(1) = data{1}.header.sr;
   model.filter.sr = data{end}.header.sr;
   [sts_prepdata, y{2}, sr(2)] = pspm_prepdata(data{1}.data, model.filter);
   if sts_prepdata == -1
     warning('ID:invalid_input', 'Call of pspm_prepdata failed.');
     return;
   end
-  y{iFile} = data{end}.data(:);
-  sr(iFile) = data{end}.header.sr;
+  y{iFile} = data{1}.data(:);
+  sr(iFile) = data{1}.header.sr;
   % 3.4 Check data units
   if ~strcmpi(data{1}.header.units, 'uS') && any(strcmpi('dcm', method))
     fprintf(['\nYour data units are stored as %s, ',...
