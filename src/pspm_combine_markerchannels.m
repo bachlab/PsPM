9bb443def7c3d531200dc6f536f317fe74c21121
teddy.chao <dadi.zhao@ucl.ac.uk>
2023-09-29 12:29:33 +0100

    reindent code (can revert this if you are unhappy)

--- src/pspm_combine_markerchannels.m
+++ src/pspm_combine_markerchannels.m
@@ -1,22 +1,22 @@
 function sts = pspm_combine_markerchannels(datafile, options)
 % ● Description
 %   pspm_combine_markerchannels combines several marker channels into one.
-%   Index of original marker channel is converted into marker name and marker 
-%   value of the new channel. 
+%   Index of original marker channel is converted into marker name and marker
+%   value of the new channel.
 %   This allows for example creating GLM timing definitions based on
 %   markers distributed across multiple channels.
 % ● Format
 %   sts = pspm_combine_markerchannels(datafile, options)
 % ● Arguments
-%    datafile:          data file name(s) (char, or cell array for multiple
-%                       files)
-%   ┌─────────options:
+%    datafile:          data file name(s) (char, or cell array for 
+%                       multiple files)
+%   ┌────────options:
 %   ├.channel_action:
-%   │             Accepted values: 'add'/'replace'
-%   │             Defines whether the new channel should be added on top of
-%   │             existing marker channels ('add), or all existing marker
-%   │             channels should be deleted and replaced with the one
-%   │             new channel ('replace')
+%   │                   Accepted values: 'add'/'replace'
+%   │                   Defines whether the new channel should be added 
+%   │                   on top of existing marker channels ('add'), or all 
+%   │                   existing marker channels should be deleted and 
+%   │                   replaced with the one new channel ('replace').
 %   └.marker_chan_num:  any number of marker channel numbers - if undefined
 %                       or 0, all marker channels of each file are used
 % ● History
@@ -46,35 +46,35 @@ if options.invalid
 end
 
 for iFile = 1:numel(datafile)
-    % combine all markers
-    [sts, infos, data, filestruct] = pspm_load_data(datafile{iFile}, options.marker_chan_num);
-    if sts < 1, return; end
-    for iChannel = 1:numel(data)
-        if ~strcmpi(data{iChannel}.header.chantype, 'marker')
-            warning('ID:invalid_input', sprintf('Channel %1.0f is not of type marker', ...
-                filestruct.posofchannels(iChannel))); return; 
-        end
+  % combine all markers
+  [sts, infos, data, filestruct] = pspm_load_data(datafile{iFile}, options.marker_chan_num);
+  if sts < 1, return; end
+  for iChannel = 1:numel(data)
+    if ~strcmpi(data{iChannel}.header.chantype, 'marker')
+      warning('ID:invalid_input', sprintf('Channel %1.0f is not of type marker', ...
+        filestruct.posofchannels(iChannel))); return;
     end
-    newdata{1}.data = sort(reshape(cell2mat(cellfun(@(x) x.data(:), data, ...
-        'UniformOutput', false)), [], 1));
-    newdata{1}.header = data{1}.header;
-    newdata{1}.markerinfo.value = reshape(cumsum(cell2mat(cellfun(@(x) ones(length(x.data), 1), ...
-        data, 'UniformOutput', false)), 2), [], 1);
-    newdata{1}.markerinfo.name = num2str(newdata{1}.markerinfo.value);
-    % decide which channels to drop
-    if strcmpi(options.channel_action, 'replace')
-        dropchannels = filestruct.posofchannels;
-    else
-        dropchannels = [];
-    end
-    % load entire file, add new channel, drop channels and save
-     [sts, infos, data] = pspm_load_data(datafile{iFile});
-     data(dropchannels) = [];
-     data(end + 1) = newdata;
-     sts = pspm_load_data(datafile{iFile}, struct('data', {data}, ...
-         'infos', infos, 'options', struct('overwrite', 1)));
-  
-     if sts < 1, return; end
+  end
+  newdata{1}.data = sort(reshape(cell2mat(cellfun(@(x) x.data(:), data, ...
+    'UniformOutput', false)), [], 1));
+  newdata{1}.header = data{1}.header;
+  newdata{1}.markerinfo.value = reshape(cumsum(cell2mat(cellfun(@(x) ones(length(x.data), 1), ...
+    data, 'UniformOutput', false)), 2), [], 1);
+  newdata{1}.markerinfo.name = num2str(newdata{1}.markerinfo.value);
+  % decide which channels to drop
+  if strcmpi(options.channel_action, 'replace')
+    dropchannels = filestruct.posofchannels;
+  else
+    dropchannels = [];
+  end
+  % load entire file, add new channel, drop channels and save
+  [sts, infos, data] = pspm_load_data(datafile{iFile});
+  data(dropchannels) = [];
+  data(end + 1) = newdata;
+  sts = pspm_load_data(datafile{iFile}, struct('data', {data}, ...
+    'infos', infos, 'options', struct('overwrite', 1)));
+
+  if sts < 1, return; end
 end
 
 
