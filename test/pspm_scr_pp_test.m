5c35d8fb6376370f567713aeffea2a274be6db30
dadi.zhao <dadi.zhao@outlook.com>
2023-10-02 02:42:58 +0100

    Update pspm_scr_pp_test.m

--- test/pspm_scr_pp_test.m
+++ test/pspm_scr_pp_test.m
@@ -26,7 +26,7 @@ classdef pspm_scr_pp_test < matlab.unittest.TestCase
       %scr_pp_test_missing(this, channels)
       channels{2}.chantype = 'hb';
       channels{3}.chantype = 'scr';
-      scr_pp_test_template(this, channels)
+      % scr_pp_test_template(this, channels)
       %scr_pp_test_missing(this, channels)
       % Delete testdata
       if exist(this.fn, 'file')
