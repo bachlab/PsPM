f21dead0514db46a62cd01cdcc6935241700c990
dadi.zhao <dadi.zhao@outlook.com>
2023-11-20 18:36:08 +0000

    update

--- src/pspm_get_smr.m
+++ src/pspm_get_smr.m
@@ -11,6 +11,7 @@ function [sts, import, sourceinfo] = pspm_get_smr(datafile, import)
 % ● History
 %   Introduced in PsPM 3.0
 %   Written in 2008-2015 by Dominik R Bach (Wellcome Trust Centre for Neuroimaging)
+%   Update in 2023 by Teddy
 
 %% 1 Initialise
 global settings
@@ -20,18 +21,15 @@ end
 sts = -1;
 sourceinfo = [];
 addpath(pspm_path('Import','SON'));
-
-%% 2 get external file
-% -------------------------------------------------------------------------
+%% 2 Get external file
 % using SON library
 warning off;
-
+% 2.1 Open file
 fid = fopen(datafile);
+% 2.2 Get channel list
 chanlist = SONChanList(fid);
-
 % preallocate memory for speed
 chandata = cell(numel(chanlist), 1);
-
 errorflag = [];
 % read channels
 for channel = 1:numel(chanlist)
@@ -41,23 +39,19 @@ for channel = 1:numel(chanlist)
     errorflag(channel)=1;
     chandata{channel}=[];
     chanhead{channel}.title='';
-  end;
-end;
+  end
+end
 fclose(fid);
-
 % delete empty channels
 if ~isempty(errorflag)
   ind=find(errorflag);
   for channel=ind(end:-1:1)
     chandata(channel)=[];
     chanhead(channel)=[];
-  end;
-end;
-
+  end
+end
 warning on;
-
 %% 3 extract individual channels
-% -------------------------------------------------------------------------
 % loop through import jobs
 for k = 1:numel(import)
   % define channel number ---
@@ -66,13 +60,14 @@ for k = 1:numel(import)
   else
     channel = pspm_find_channel(arrayfun(@(i) chanhead{i}.title, 1:numel(chanhead), 'UniformOutput', 0), ...
       import{k}.type); % bring channel names into a cell array
-    if channel < 1, return; end;
-  end;
-
-  if channel > numel(chandata), warning('ID:channel_not_contained_in_file', 'Channel %02.0f not contained in file %s.\n', channel, datafile); return; end;
-
+    if channel < 1, return; end
+  end
+  if channel > numel(chandata)
+    warning('ID:channel_not_contained_in_file', ...
+      'Channel %02.0f not contained in file %s.\n', channel, datafile); 
+    return; 
+  end
   sourceinfo.channel{k, 1} = sprintf('Channel %02.0f: %s', channel, chanhead{channel}.title);
-
   % convert to waveform or get sample rate for wave channel types
   if strcmpi(settings.channeltypes(import{k}.typeno).data, 'wave')
     if chanhead{channel}.kind == 1 % waveform
@@ -90,24 +85,29 @@ for k = 1:numel(import)
       % start with low to high
       if chanhead{channel}.initLow==0
         pulse(1)=[];
-      end;
+      end
       pulse = pulse(1:2:end);
       import{k}.data = pspm_pulse_convert(pulse, settings.import.rsr, settings.import.sr);
       import{k}.sr = settings.import.sr;
       import{k}.minfreq = min(import{k}.data);
     else
       warning('Unknown channel format in CED spike file for import job %02.0f', k);  return;
-    end;
+    end
     % extract, and possibly denoise event channels
   elseif strcmpi(settings.channeltypes(import{k}.typeno).data, 'events')
     if chanhead{channel}.kind == 1 % waveform
       import{k}.marker = 'continuous';
       import{k}.data = chandata{channel};
       import{k}.sr   = 1./chanhead{channel}.sampleinterval;
-    elseif chanhead{channel}.kind == 4 && strcmpi(import{k}.type, 'marker') % for TTL marker channels with up AND down timestamps
+    elseif chanhead{channel}.kind == 4 && strcmpi(import{k}.type, 'marker') 
+      % for TTL marker channels with up AND down timestamps
       kbchan = pspm_find_channel(arrayfun(@(i) chanhead{i}.title, 1:numel(chanhead), 'UniformOutput', 0), ...
         {'keyboard'}); % keyboard channel doesn't exist by default but is needed for denoising
-      if kbchan > 0, kbdata = chandata{kbchan}; else kbdata = []; end;
+      if kbchan > 0
+        kbdata = chandata{kbchan}; 
+      else 
+        kbdata = []; 
+      end
       if isfield(import{k}, 'denoise') && ~isempty(import{k}.denoise) && import{k}.denoise > 0
         import{k}.data = pspm_denoise_spike(chandata{channel}, chanhead{channel}, kbdata, import{k}.denoise);
       else
@@ -115,21 +115,19 @@ for k = 1:numel(import)
         % start with low to high
         if chanhead{channel}.initLow==0
           pulse(1)=[];
-        end;
+        end
         import{k}.data = pulse(1:2:end);
-      end;
+      end
       import{k}.sr = 0.001; % milliseconds import for marker channels, see above
       import{k}.marker = 'timestamp';
     else        % for TTL channels with up OR down timestamps
       import{k}.data = chandata{channel};
       import{k}.sr = 0.001; % milliseconds import for marker channels, see above
       import{k}.marker = 'timestamp';
-    end;
-  end;
-end;
-
+    end
+  end
+end
 %% 4 Clear path and return
-% -------------------------------------------------------------------------
 rmpath(pspm_path('Import','SON'));
 sts = 1;
 return
--- src/pspm_get_smrx.m
+++ src/pspm_get_smrx.m
@@ -17,10 +17,58 @@ end
 sts = -1;
 sourceinfo = [];
 addpath(pspm_path('Import','CEDS64ML'));
-
 %% 2 Get external file
-% using CED library
-warning off;
-fid = fopen(datafile);
-chanlist = SONChanList(fid);
+% 2.1 Open file
+fhand = CEDS64Open(datafile, 1);
+if (fhand < 0); error('Could not open file.'); end
+% 2.2 Read file info
+hdr.timebase      = CEDS64TimeBase(fhand);
+hdr.maxchan       = CEDS64MaxChan(fhand);
+hdr.maxtime       = CEDS64MaxTime(fhand);
+[~, hdr.timedate] = CEDS64TimeDate(fhand);
+hdr.timedate = double(hdr.timedate);
+% 2.3 Get list of channels
+iChan = 0;
+iMarkerChan = [];
+MarkerChanName = {};
+for i = 1:hdr.maxchan
+  chanType = CEDS64ChanType(fhand, i); % Read channel type
+  switch chanType % Check type of the channel
+    case {1,9} % ADC channels: read as signals
+      iChan = iChan + 1;
+      hdr.chaninfo(iChan).number = i;
+      hdr.chaninfo(iChan).kind = chanType;
+      [~, hdr.chaninfo(iChan).title] = CEDS64ChanTitle(fhand, i);
+      % [isOk, hdr.chaninfo(iChan).comment] = CEDS64ChanComment(fhand, i);
+      hdr.chaninfo(iChan).div = CEDS64ChanDiv(fhand, i);
+      hdr.chaninfo(iChan).idealRate = CEDS64IdealRate(fhand, i);
+      hdr.chaninfo(iChan).realRate = 1 ./ (hdr.timebase .* hdr.chaninfo(iChan).div);
+      % Convert units to gain
+      [~, hdr.chaninfo(iChan).units] = CEDS64ChanUnits(fhand, i);
+      chUnits = lower(strtrim(hdr.chaninfo(iChan).units));
+      if ~isempty(chUnits)
+        if contains(chUnits, 'μ') || contains(chUnits, 'micro')
+          hdr.chaninfo(iChan).gain = 1e-6;
+        elseif contains(chUnits, 'milli') || contains(chUnits, 'mv')
+          hdr.chaninfo(iChan).gain = 1e-3;
+        else
+          hdr.chaninfo(iChan).gain = 1;
+        end
+      end
+    case {2,3,4,5,6,7,8} % Markers and events
+      iMarkerChan(end+1) = i;
+      [~, chTitle] = CEDS64ChanTitle(fhand, i);
+      chTitle = str_remove_spec_chars(chTitle);
+      if ~isempty(chTitle)
+        MarkerChanName{end+1} = chTitle;
+      else
+        MarkerChanName{end+1} = num2str(i);
+      end
+  end
+end
+hdr.nchan = length(hdr.chaninfo);
+% 2.4 Get maximum sampling rate
+[sfreq, chMax] = max([hdr.chaninfo.idealRate]);
+
+
 return
