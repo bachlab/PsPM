7264cab88a65fea6cda09b61b2cb7a20d179b05a
dadi.zhao <dadi.zhao@outlook.com>
2023-12-18 19:17:38 +0000

    update

--- src/pspm_cfg/pspm_cfg_run_interpolate.m
+++ src/pspm_cfg/pspm_cfg_run_interpolate.m
@@ -1,10 +1,7 @@
 function out = pspm_cfg_run_interpolate(job)
-
 % Updated 18-12-2023 by Teddy
-
 options = struct();
 fn = job.datafiles;
-
 if isfield(job.mode, 'file')
     options.overwrite = job.mode.file.overwrite;
     options.newfile = true;
@@ -12,15 +9,11 @@ elseif isfield(job.mode, 'channel')
     options.channels = cell(size(job.datafiles));
     options.channels{:} = job.mode.channel.source_chan;
     options.newfile = false;
-
     if isfield(job.mode.channel.mode, 'new_chan')
         options.channel_action = 'add';
     elseif isfield(job.mode.channel.mode, 'replace_chan')
         options.channel_action = 'replace';
-    end;
-
-end;
-
+    end
+end
 options.extrapolate = job.extrapolate;
-
 [~, out] = pspm_interpolate(fn, options);
