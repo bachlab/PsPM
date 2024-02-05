6efaa520c2873d0dbfd76fdbd458d7da2dae10b7
Dadi Zhao (Cancer and Genomic Sciences) <dadi.zhao@outlook.com>
2024-02-05 03:30:09 +0000

    fix stupid bugs

--- src/pspm_cfg/pspm_update_struct.m
+++ src/pspm_cfg/pspm_update_struct.m
@@ -2,19 +2,14 @@ function StructY = pspm_update_struct(StructY, StructX, Fields)
 if ~exist('StructY','var')
   StructY = struct();
 end
-switch length(Fields)
-case 1
-	if isfield(StructX, Fields)
-		FieldValue = getfield(StructX, Fields);
-		StructY = setfield(StructY, Fields, FieldValue);
-	end
-otherwise
-	for iField = 1:length(Fields)
-		Field = Fields{iField};
-		if isfield(StructX, Field)
-			FieldValue = getfield(StructX, Field);
-			StructY = setfield(StructY, Field, FieldValue);
-		end
+if ~iscell(Fields)
+  Fields = {Fields};
+end
+for iField = 1:length(Fields)
+	Field = Fields{iField};
+	if isfield(StructX, Field)
+		FieldValue = getfield(StructX, Field);
+		StructY = setfield(StructY, Field, FieldValue);
 	end
 end
 end
