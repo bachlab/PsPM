51d34f6d75d601c21113047c441f7ba9b627941a
Dominik Bach <dominik.bach@uzh.ch>
2024-02-16 06:19:35 +0100

    revert all side changes

--- src/pspm_cfg/pspm_cfg_run_glm_sebr.m
+++ src/pspm_cfg/pspm_cfg_run_glm_sebr.m
@@ -1,48 +1,35 @@
 function out = pspm_cfg_run_glm_sebr(job)
-% Executes pspm_glm
-
-% $Id$
-% $Rev$
-
+% Updated on 08-01-2024 by Teddy
 global settings
 if isempty(settings), pspm_init; end
-
 % set modality
 modality = 'sebr';
 modelspec = 'sebr';
-
 f = strcmpi({settings.glm.modelspec}, modelspec);
 def_filter = settings.glm(f).filter;
-
 params = pspm_cfg_run_glm(job, def_filter);
-
 % get parameters
 model = params.model;
 options = params.options;
-
 % basis function
 rf = fieldnames(job.bf.rf);
 rf = rf{1};
-
 if any(strcmp(rf,{'sebrf0', 'sebrf1'}))
-    model.bf.fhandle = str2func('pspm_bf_sebrf');
-
-    if strcmp(rf, 'sebrf0')
-        model.bf.args = [];
-    elseif strcmp(rf, 'sebrf1')
-        model.bf.args = 1;
-    end
+  model.bf.fhandle = str2func('pspm_bf_sebrf');
+  if strcmp(rf, 'sebrf0')
+    model.bf.args = [];
+  elseif strcmp(rf, 'sebrf1')
+    model.bf.args = 1;
+  end
 end
-
 % set modality
 model.modality = modality;
 model.modelspec = modelspec;
-
 out = pspm_glm(model, options);
 if exist('out', 'var') && isfield(out, 'modelfile')
-    if ~iscell(out.modelfile)
-        out.modelfile ={out.modelfile};
-    end
+  if ~iscell(out.modelfile)
+    out.modelfile ={out.modelfile};
+  end
 else
-    out(1).modelfile = cell(1);
+  out(1).modelfile = cell(1);
 end
