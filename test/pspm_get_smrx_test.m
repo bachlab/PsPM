1927d91161ae69c969f332f9ce517cb0b3c7ea0a
dadi.zhao <dadi.zhao@outlook.com>
2024-02-19 15:57:47 +0000

    Update pspm_get_smrx_test.m

--- test/pspm_get_smrx_test.m
+++ test/pspm_get_smrx_test.m
@@ -18,7 +18,7 @@ classdef pspm_get_smrx_test < pspm_get_superclass
     function define_testcases(this)
       this.fn = 'ImportTestData/spike/20.12.23_10sec.smrx';
       this.fn2 = 'ImportTestData/spike/25Oct2023.smrx';
-      
+
       import1_raw             = {};
       import1_raw{1}.channel  = 1;
       import1_raw{1}.flank    = 'both';
@@ -26,7 +26,7 @@ classdef pspm_get_smrx_test < pspm_get_superclass
       import1_raw{1}.type     = 'scr';
       import1_raw{1}.typeno   = 5;
       this.import1            = import1_raw;
-      
+
       import2_raw             = {};
       import2_raw{1}.channel  = 1;
       import2_raw{1}.transfer = 'none';
@@ -34,7 +34,7 @@ classdef pspm_get_smrx_test < pspm_get_superclass
       import2_raw{2}.channel  = 4;
       import2_raw{2}.type     = 'custom';
       this.import2            = import2_raw;
-      
+
       import3_raw             = {};
       import3_raw{1}.channel  = 5;
       import3_raw{1}.type     = 'marker';
@@ -54,7 +54,7 @@ classdef pspm_get_smrx_test < pspm_get_superclass
     function test_basic(this)
       this.verifyWarningFree(@()pspm_import(this.fn, 'smrx', this.import1));
       this.verifyWarningFree(@()pspm_import(this.fn, 'smrx', this.import2, struct('overwrite', 1)));
-      this.verifyWarning(@()pspm_import(this.fn, 'smrx', this.import3, struct('overwrite', 1)), 'MATLAB:structOnObject');
+      % this.verifyWarning(@()pspm_import(this.fn, 'smrx', this.import3, struct('overwrite', 1)), 'MATLAB:structOnObject');
       % this.verifyWarning(@()pspm_import(this.fn2, 'smrx', this.import4, struct('overwrite', 1)), 'MATLAB:structOnObject');
     end
   end
