e2e16642f6fce1203af2a84576cdd16c56e1dbc6
Dominik Bach <dbach@uni-bonn.de>
2024-02-22 12:45:58 +0100

    clean up smrx import

--- src/pspm_get_smrx.m
+++ src/pspm_get_smrx.m
@@ -40,18 +40,19 @@ function [sts, import, sourceinfo] = pspm_get_smrx(datafile, import)
 %% 1 Initialise
 global settings
 if isempty(settings)
-  pspm_init;
+    pspm_init;
 end
 sts = -1;
 sourceinfo = [];
 addpath(pspm_path('Import','CEDS64ML'));
 if ~strcmpi(computer('arch'), 'win64')
-  error('Reading .smrx files is available only on Windows 64bit.');
+    error('Reading .smrx files is available only on Windows 64bit.');
 end
 % Add path to CED code
 cedpath = fileparts(which('CEDS64Open'));
 setenv('CEDS64ML', fileparts(which('CEDS64Open')));
 CEDS64LoadLib(cedpath);
+maxEvents = 1e5; % this is a hardcoded value for maximum number of events import
 %% 2 Get external file
 warning off;
 % 2.1 Open file
@@ -65,137 +66,115 @@ fileinfo.maxtime       = CEDS64MaxTime(fhand);
 fileinfo.timedate      = double(fileinfo.timedate);
 % 2.3 Get list of channels
 iChan = 0;
-chanindx = [];
-for i = 1:fileinfo.maxchan
-  chanType = CEDS64ChanType(fhand, i); % Read channel type
-  switch chanType % Check type of the channel
-    case {1,9} % ADC channels: read as signals
-      iChan                             = iChan + 1;
-      fileinfo.chaninfo(iChan).kind     = chanType;
-      X                                 = GetCEDChanInfo(fhand, i);
-      for fn = fieldnames(X)'; fileinfo.chaninfo(iChan).(fn{1}) = X.(fn{1}); end % transfer to fileinfo
-      fileinfo.chaninfo(iChan).realRate = 1 ./ (fileinfo.timebase .* fileinfo.chaninfo(iChan).div);
-      chanindx = [chanindx, iChan];
-    case {2,3,4,5,6,7,8} % Markers and events
-      iChan = iChan + 1;
-      fileinfo.chaninfo(iChan).kind     = chanType;
-      X                                 = GetCEDChanInfo(fhand, i);
-      for fn = fieldnames(X)'; fileinfo.chaninfo(iChan).(fn{1}) = X.(fn{1}); end % transfer to fileinfo
-      chanindx = [chanindx, iChan];
-    otherwise
-      iChan                             = iChan + 1;
-      fileinfo.chaninfo(iChan).number   = i;
-      fileinfo.chaninfo(iChan).kind     = 0;
-      fileinfo.chaninfo(iChan).title    = '0';
-  end
+chanindx = []; % index of non-empty channels
+for iChannel = 1:fileinfo.maxchan
+    chanType = CEDS64ChanType(fhand, iChannel); % Read channel type
+    if ismember(chanType, 1:9)
+        iChan                             = iChan + 1;
+        X                                 = GetCEDChanInfo(fhand, iChannel);
+        for fn = fieldnames(X)'; fileinfo.chaninfo(iChan).(fn{1}) = X.(fn{1}); end % transfer to fileinfo
+        fileinfo.chaninfo(iChan).type = chanType;
+        chanindx = [chanindx, iChan];
+    else
+        iChan                             = iChan + 1;
+        fileinfo.chaninfo(iChan).number   = iChannel;
+        fileinfo.chaninfo(iChan).type     = 0;
+        fileinfo.chaninfo(iChan).title    = '0';
+    end
 end
 fileinfo.nchan = length(fileinfo.chaninfo);
-% 2.4 Get maximum sampling rate
-[~, ~] = max([fileinfo.chaninfo.idealRate]);
-warning on;
+
+% channel types per CEDS64ChanType.m:
+%   iType - 0 no channel
+%           1 Waveform channel
+%           2 Event (falling)
+%           3 Event (rising)
+%           4 Event (both)
+%           5 Marker
+%           6 Wavemark
+%           7 Realmark
+%           8 TextMark
+%           9 Realwave
+%           or a negative error code
+
 %% 3 Extract individual channels
 % 3.1 Loop through import jobs
 for iImport = 1:numel(import)
-  % 3.1.1 define channel number
-  if import{iImport}.channel > 0
-    channel = chanindx(import{iImport}.channel);
-  else
-    channel = pspm_find_channel(fileinfo.chaninfo.kind,...
-      import{iImport}.type);
-    if channel < 1
-      warning('ID:channel_not_contained_in_file', ...
-        'Channel %02.0f not contained in file %s.\n', channel, datafile);
-      return;
+    % 3.1.1 define channel number
+    if import{iImport}.channel > 0
+        channel = chanindx(import{iImport}.channel);
+    else
+        [sts, channel] = pspm_find_channel({fileinfo.chaninfo.kind},...
+            import{iImport}.type);
+        if sts < 1, return; end
     end
-  end
-  if channel > fileinfo.nchan
-    warning('ID:channel_not_contained_in_file', ...
-      'Channel %02.0f not contained in file %s.\n', channel, datafile);
-    return;
-  end
-  sourceinfo.channel{iImport, 1} = sprintf('Channel %02.0f: %s', channel, fileinfo.chaninfo.title);
-  % 3.1.2 convert to waveform or get sample rate for wave channel types
-  switch settings.channeltypes(import{iImport}.typeno).data
-    case 'wave'
-      switch fileinfo.chaninfo(channel).kind
+    if channel > fileinfo.nchan
+        warning('ID:channel_not_contained_in_file', ...
+            'Channel %02.0f not contained in file %s.\n', channel, datafile);
+        return;
+    end
+    sourceinfo.channel{iImport, 1} = sprintf('Channel %02.0f: %s', channel, fileinfo.chaninfo.title);
+    % 3.1.2 read individual channels
+    switch fileinfo.chaninfo(channel).type
         case 0 % empty
-          warning('ID:empty_channel', 'The specified channel was not recorded. \n');
-          return
+            warning('ID:empty_channel', 'The specified channel was not recorded. \n');
+            return
         case 1 % waveform
-          dataLength                = floor(fileinfo.maxtime/fileinfo.chaninfo(channel).div);
-          [nWF, dataWF]             = CEDS64ReadWaveF(fhand, channel, dataLength, 1);
-          import{iImport}.data      = dataWF;
-          import{iImport}.length    = nWF;
-          import{iImport}.sr        = fileinfo.chaninfo(channel).realRate;
-          import{iImport}           = InheritFields(import{iImport}, fileinfo.chaninfo(channel));
-        case 3 % time stamps
-          dataLength                = floor(fileinfo.maxtime/fileinfo.chaninfo(channel).div);
-          [nWF, dataWF]             = CEDS64ReadWaveF(fhand, channel, dataLength, 1);
-          import{iImport}.data      = pspm_pulse_convert(dataWF, settings.import.rsr, settings.import.sr);
-          import{iImport}.length    = nWF;
-          import{iImport}.sr        = settings.import.sr;
-          import{iImport}           = InheritFields(import{iImport}, fileinfo.chaninfo(channel));
-        case 4 % up and down time stamps
-          dataLength                = floor(fileinfo.maxtime/fileinfo.chaninfo(channel).div);
-          [nTS, dataTS]             = CEDS64ReadWaveF(fhand, channel, dataLength, 1);
-          if fileinfo.chaninfo(iChan).initLow == 0; dataTS(1) = []; end
-          dataTS                    = dataTS(1:2:end);
-          import{iImport}.data      = pspm_pulse_convert(dataTS, settings.import.rsr, settings.import.sr);
-          import{iImport}.length    = nTS;
-          import{iImport}.sr        = settings.import.sr;
-          import{iImport}           = InheritFields(import{iImport}, fileinfo.chaninfo(channel));
+            dataLength                = floor(fileinfo.maxtime/fileinfo.chaninfo(channel).div);
+            [nWF, dataWF]             = CEDS64ReadWaveF(fhand, channel, dataLength, 1);
+            import{iImport}.data      = dataWF;
+            import{iImport}.length    = nWF;
+            import{iImport}.sr        = fileinfo.chaninfo(channel).actualRate;
+
+        case {2, 3} % event falling/rising
+            warning('ID:untested_feature', 'The specified channel type is of untested type. Proceed at your own risk. Please reach out to PsPM developers with test data. \n');
+            [nEvents, dataEvents]     = CEDS64ReadEvents(fhand, channel, maxEvents, 1, fileinfo.maxtime );
+
+        case 4 % event both - convert to waveform so that flanks can be handled in pspm_get_events
+            [ iRead, i64Times, iLevel ] = CEDS64ReadLevels( fhand, channel, maxEvents, 1, fileinfo.maxtime );
+
+            if iLevel == 1
+                i64Times = [1; i64Times];
+            end
+            if mod(length(i64Times), 2) == 1
+                i64Times = [i64Times; fileinfo.maxtime];
+            end
+            i64Times = reshape(i64Times, 2, [])';
+            sr = 1./CEDS64TicksToSecs(fhand, 1);
+            index = pspm_epochs2logical(i64Times, fileinfo.maxtime, 1); % epochs are specified in samples, so sr = 1 (see pspm_epochs2logical)   
+            import{iImport}.data      = index;
+            import{iImport}.length    = numel(index);
+            import{iImport}.sr        = sr;
+
+        case 5 % marker
+            [nEvents, dataMarkers]     = CEDS64ReadMarkers(fhand, channel, maxEvents, 1, fileinfo.maxtime );
+            import{iImport}.markerinfo.value           = double([dataMarkers(:,1).m_Code1]);
+            import{iImport}.markerinfo.name           = cellfun(@num2str, num2cell([dataMarkers(:,1).m_Code1]), 'UniformOutput', false);
+            dataEvents = double([dataMarkers(:,1).m_Time]);
+
         otherwise
-          warning('ID:feature_unsupported', 'The specified channel is of unsupported type. \n');
-          return
-      end
-    case 'events'
-      switch fileinfo.chaninfo(channel).kind
-        case 0 % empty
-          warning('ID:empty_channel', 'The specified event channel was not recorded. \n');
-          return
-        case 1 % Events
-          dataLength                = floor(fileinfo.maxtime/fileinfo.chaninfo(channel).div);
-          [nEvents, dataEvents]     = CEDS64ReadEvents(fhand, channel, dataLength, 1);
-          import{iImport}.marker    = 'continuous';
-          import{iImport}.data      = dataEvents;
-          import{iImport}.length    = nEvents;
-          switch ~isempty(fileinfo.chaninfo(channel).realRate)
-            case 0
-              import{iImport}.sr    = fileinfo.chaninfo(channel).idealRate;
-            case 1
-              import{iImport}.sr    = fileinfo.chaninfo(channel).realRate;
-          end
-          import{iImport}           = InheritFields(import{iImport}, fileinfo.chaninfo(channel));
-        case 3 % time stamps
-          % waiting for test data
-          warning('ID:feature_unsupported', 'Feature of reading time stamps has not been available. \n');
-          return
-        case 4 % For events and type 4 channel, to read as marker channels
-          dataLength = floor(fileinfo.maxtime * fileinfo.timebase * fileinfo.chaninfo(iChan).idealRate);
-          [nMarker, dataMarker]     = CEDS64ReadMarkers(fhand, channel, dataLength, 1);
-          import{iImport}.marker    = 'timestamp';
-          import{iImport}.data      = double([dataMarker(:,1).m_Time]);
-          import{iImport}.length    = nMarker;
-          switch ~isempty(fileinfo.chaninfo(channel).realRate)
-            case 0
-              import{iImport}.sr    = fileinfo.chaninfo(channel).idealRate;
-            case 1
-              import{iImport}.sr    = fileinfo.chaninfo(channel).realRate;
-          end
-          import{iImport}           = InheritFields(import{iImport}, fileinfo.chaninfo(channel));
-        otherwise
-          dataLength = floor(fileinfo.maxtime * fileinfo.timebase * fileinfo.chaninfo(iChan).idealRate);
-          [nMarker, dataMarker]     = CEDS64ReadMarkers(fhand, channel, dataLength, 1);
-          import{iImport}.data      = dataMarker;
-          import{iImport}.length    = nMarker;
-          import{iImport}.sr        = 0.001; % milliseconds import for marker channels, see above
-          import{iImport}.marker    = 'timestamp';
-          import{iImport}           = InheritFields(import{iImport}, fileinfo.chaninfo(channel));
-      end
-    otherwise
-      warning('ID:feature_unsupported', 'The specified channel is of unsupported type. \n');
-      return
-  end
+            % waiting for test data
+            warning('ID:feature_unsupported', 'The specified channel is of unsupported type. Please contact the PsPM team with example data.  \n');
+            return
+    end
+
+    if ismember(fileinfo.chaninfo(channel).type, [2, 3, 5])
+        import{iImport}.data      = dataEvents*CEDS64TicksToSecs(fhand, 1); % convert to seconds
+        import{iImport}.length    = nEvents;
+        import{iImport}.sr        = 1;
+    end
+
+    if strcmpi(settings.channeltypes(import{iImport}.typeno).data, 'events')
+        if ismember(fileinfo.chaninfo(channel).type, [1, 4])
+            import{iImport}.marker = 'continuous';
+        else
+            import{iImport}.marker = 'timestamps';
+        end
+    elseif fileinfo.chaninfo(channel).type > 1
+        % event data for wave channels is supported for smr but we have no smrx test data
+        warning('ID:feature_unsupported', 'Pulse rate import is not supported for smrx files. Please contact the PsPM team with example data. \n');
+        return
+    end
 end
 %% 4 Clear path and return
 rmpath(pspm_path('Import','CEDS64ML'));
@@ -209,22 +188,22 @@ function Y = InheritFields(Y, X)
 % ● History
 %   Written in 2024 by Teddy
 if isfield(X, 'div')
-  Y.div = X.div;
+    Y.div = X.div;
 end
 if isfield(X, 'gain')
-  Y.gain = X.gain;
+    Y.gain = X.gain;
 end
 if isfield(X, 'idealRate')
-  Y.idealRate = X.idealRate;
+    Y.idealRate = X.idealRate;
 end
 if isfield(X, 'number')
-  Y.number = X.number;
+    Y.number = X.number;
 end
 if isfield(X, 'title')
-  Y.title = X.title;
+    Y.title = X.title;
 end
 if isfield(X, 'units')
-  Y.units = X.units;
+    Y.units = X.units;
 end
 function Y = GetCEDChanInfo(fhand, i)
 % ● Description
@@ -238,21 +217,22 @@ function Y = GetCEDChanInfo(fhand, i)
 [~, Y.comment]  = CEDS64ChanComment(fhand, i);
 Y.div           = CEDS64ChanDiv(fhand, i);
 Y.idealRate     = CEDS64IdealRate(fhand, i);
+Y.actualRate    = 1./CEDS64TicksToSecs(fhand, Y.div);
 Y.number        = i;
 [~, chTitle]    = CEDS64ChanTitle(fhand, i);
 if ~isempty(chTitle)
-  Y.title = chTitle;
+    Y.title = chTitle;
 else
-  Y.title = num2str(i);
+    Y.title = num2str(i);
 end
 [~, Y.units]    = CEDS64ChanUnits(fhand, i); % Convert units to gain
 chUnits = lower(strtrim(Y.units));
 if ~isempty(chUnits)
-  if contains(chUnits, 'μ') || contains(chUnits, 'micro')
-    Y.gain = 1e-6;
-  elseif contains(chUnits, 'milli') || contains(chUnits, 'mv')
-    Y.gain = 1e-3;
-  else
-    Y.gain = 1;
-  end
+    if contains(chUnits, 'μ') || contains(chUnits, 'micro')
+        Y.gain = 1e-6;
+    elseif contains(chUnits, 'milli') || contains(chUnits, 'mv')
+        Y.gain = 1e-3;
+    else
+        Y.gain = 1;
+    end
 end
