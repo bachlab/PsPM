b7131572cc1d3771be7d7b113762c84cb52f8f20
Abdul Wahab Madni <105889425+MadniAbdulWahab@users.noreply.github.com>
2024-02-27 11:40:37 +0100

    Update pspm_check_python_modules.m

--- src/pspm_check_python_modules.m
+++ src/pspm_check_python_modules.m
@@ -1,28 +1,33 @@
-function pspm_check_python_modules()
-    % pspm_check_python_modules Lists currently imported Python modules in MATLAB.
+function moduleNames = pspm_check_python_modules()
+    % pspm_check_python_modules Returns a list of currently imported Python modules in MATLAB.
     %
-    % This function retrieves and displays the names of Python modules that have been imported
-    % into the current MATLAB session.
+    % This function retrieves and returns the names of Python modules that have been imported
+    % into the current MATLAB session. 
+    % It checks the status of the Python environment in MATLAB
+    % and provides informative feedback to the user based on the Python setup status.
 
-    % Ensure Python is correctly set up in MATLAB
-    if strcmp(pyenv().Version, "")
-        fprintf('Python environment does not exists. Run ''pspm_check_python()'' to ensure that the environment is correctly set up.\n');
-        return;
-    elseif ~strcmp(pyenv().Status, "Loaded")
-        fprintf('No module is loaded yet. \n')
-        return;
-    end
-    % Get the dictionary of imported Python modules
-    modulesDict = py.sys.modules;
-    
-    % Convert the Python dictionary keys (module names) to a MATLAB cell array
-    moduleNamesList = py.list(modulesDict.keys()); 
-    moduleNames = cell(moduleNamesList); 
-    
-    % Display the imported module names
-    fprintf('Imported Python Modules:\n');
-    for i = 1:length(moduleNames)
-        moduleName = char(moduleNames{i}); 
-        fprintf('%d. %s\n', i, moduleName);
+    % Initialize return variable
+    moduleNames = {};
+
+    try
+        % Ensure Python is correctly set up in MATLAB
+        pe = pyenv();
+        if strcmp(pe.Version, "")
+            fprintf('Python environment does not exist. Run ''pspm_check_python()'' to ensure that the environment is correctly set up.\n');
+            return;
+        elseif ~strcmp(pe.Status, "Loaded")
+            fprintf('No module is loaded yet.\n');
+            return;
+        else
+            % Get the dictionary of imported Python modules
+            modulesDict = py.sys.modules;
+            moduleNamesList = py.list(modulesDict.keys());
+            moduleNames = cell(moduleNamesList);
+
+            % Print confirmation message
+            fprintf('Python modules have been successfully retrieved. Total modules: %d\n', numel(moduleNames));
+        end
+    catch ME
+        fprintf('An error occurred while retrieving Python modules: %s\n', ME.message);
     end
 end
