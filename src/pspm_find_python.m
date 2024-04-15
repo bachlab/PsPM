f4158506a13c7b236bb0559db2cbf81a2636290c
dadi.zhao <dadi.zhao@outlook.com>
2024-04-15 14:12:25 +0100

    add python detect code

---
+++ src/pspm_find_python.m
@@ -0,0 +1,20 @@
+function pyinfo = pspm_find_python()
+% ● Description
+%   pspm_find_python detects python environment in the system and return
+%   the path and version of python
+% ● Description
+
+pyrunfile("py_find_location.py")
+pyinfo_text   = fileread('py_loc.txt');
+disp(pyinfo_text);
+pyinfo_struct = regexp(pyinfo_text, '\n', 'split');
+pyinfo        = pyinfo_struct(1:2);
+
+% Adjustments
+if isunix
+  pyinfo{1} = [pyinfo{1}, '/python', pyinfo{2}];
+else
+  pyinfo{1} = [pyinfo{1}, '\python', pyinfo{2}, '.exe'];
+end
+
+end
