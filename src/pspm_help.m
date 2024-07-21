ef2a4754d15bd031e7c90233dbae80b4e26d01da
Dadi Zhao (Cancer and Genomic Sciences) <dadi.zhao@outlook.com>
2024-07-21 15:45:26 +0100

    sort arguments

--- src/pspm_help.m
+++ src/pspm_help.m
@@ -36,52 +36,13 @@ A = A(cellfun(@ischar,A) & ~cellfun(@isempty,A));
 B = regexp(A,'^\s*%.*','match');
 B = vertcat(B{:});
 information = sort_info (B);
-information.Arguments = sort_args(information.Arguments);
-information.References = sort_refs(information.References);
+if isfield(information, 'Arguments')
+  information.Arguments = sort_args(information.Arguments);
+end
+if isfield(information, 'References')
+  information.References = sort_refs(information.References);
+end
 return
-
-function A = sort_args_old (B)
-% remove '% '
-for i_line = 1:length(B)
-  C = B{i_line, 1};
-  if contains(C,'%')
-    C(strfind(C,'%'))=[];
-  end
-  B{i_line, 1} = C;
-end
-% sort
-N_target = find(strcmp(B, ' ● Arguments'),1);
-str = '';
-while ( ~strcmp(B{N_target+1,1}(1),'●') )
-  str = [str, B{N_target+1, 1}, newline];
-  N_target = N_target + 1;
-  if strcmp(B{N_target+1,1}(1:2),' ●')
-    break
-  elseif strcmp(B{N_target+1,1}(1),'%')
-    break
-  elseif strcmp(B{N_target+1,1}(1:2),' 1')
-    break
-  end
-end
-D = remove_multiple_space(str);
-idx_var = strfind(D,': ');
-idx_fst = strfind(D,'. ');
-A = cell(length(idx_var), 2);
-for i_var = 1:length(idx_var)
-  switch i_var
-    case 1
-      A{1,1} = D(1:(idx_var(1)-1));
-      A{1,2} = D((idx_var(1)+2):idx_fst(1));
-      % A.(var_name) = var_disc;
-    case length(idx_var)
-      A{length(idx_var),1} = D((idx_fst(end)+2):(idx_var(end)-1));
-      A{length(idx_var),2} = D((idx_var(end)+2):end);
-      % A.(var_name) = var_disc;
-    otherwise
-      A{i_var,1} = D((idx_fst(i_var-1)+2):(idx_var(i_var)-1));
-      A{i_var,2} = D((idx_var(i_var)+2):idx_fst(i_var));
-  end
-end
 
 function A = sort_info (B)
 % remove '% '
@@ -138,19 +99,40 @@ checklist_valid = [strfind(B,[newline,'├']),...
 checklist = checklist(~ismember(checklist, checklist_valid));
 B(checklist) = '';
 B(strfind(B,'─'))='';
-[var_name_start,var_name_end] = regexp(B,'(?<=├.)(.*?)(?=:)'); % get subfields
-[var_name_start2,var_name_end2] = regexp(B,'(?<=:)(.*?)(?=\n)'); % get explainations
+
+levels = B([1,strfind(B,newline)+1]);
+level_ends = [strfind(B,newline),length(B)];
+level_starts = [1,level_ends(1:end-1)+1];
 args = struct();
-for i = 1:length(var_name_start)
-  varname = B(var_name_start(i):var_name_end(i));
-  content = B(var_name_start2(i):var_name_end2(i));
-  while content(1)==' ' 
-    content = content(2:end);
+for i_level = 1:length(levels)
+  C = B((level_starts(i_level)+1):(level_ends(i_level)-1));
+  if ~strcmp(levels(i_level),'├') && ~strcmp(levels(i_level),'└')
+    % this is a field
+    if strcmp(levels(i_level),'┌')
+      fieldname = C;
+      fieldcontent = '';
+    else
+      split = strfind(C, ':');
+      fieldname = C(1:split);
+      fieldcontent = C((split+1),end);
+    end
+    args.(fieldname) = fieldcontent;
+  else
+    [var_name_start,var_name_end] = regexp(C,'(?<=^)(.*?)(?=:)'); % get subfields
+    [var_name_start2,var_name_end2] = regexp(C,'(?<=:)(.*?)(?=$)'); % get explainations
+    varname = C(var_name_start:var_name_end);
+    content = C(var_name_start2:var_name_end2);
+    while varname(1)==' ' || varname(1)=='.'
+      varname = varname(2:end);
+    end
+    while content(1)==' ' || content(1)=='.'
+      content = content(2:end);
+    end
+    while content(end)~='.' 
+      content(end+1) = '.';
+    end
+    args.(fieldname).(varname) = content;
   end
-  while content(end)~='.' 
-    content(end+1) = '.';
-  end
-  args.(varname) = content;
 end
 
 function B = sort_refs(A)
