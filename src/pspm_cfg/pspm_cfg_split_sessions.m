dfaee626419264675816e58f4bc308954b7da168
Dominik Bach <dominik.bach@uzh.ch>
2024-08-06 15:11:25 +0200

    modify GUI for pspm_split_sessions

--- src/pspm_cfg/pspm_cfg_split_sessions.m
+++ src/pspm_cfg/pspm_cfg_split_sessions.m
@@ -13,8 +13,7 @@ split_auto          = cfg_const;
 split_auto.name     = 'Automatic';
 split_auto.tag      = 'auto';
 split_auto.val      = {0};
-split_auto.help     = {['Detect sessions according to longest distances ', ...
-'between markers.']};
+split_auto.help     = {};
 
 %% split manual
 split_manual        = cfg_entry;
@@ -22,7 +21,7 @@ split_manual.name   = 'Marker';
 split_manual.tag    = 'marker';
 split_manual.strtype = 'i';
 split_manual.num    = [1 inf];
-split_manual.help   = {'Split sessions according to given marker id''s.'};
+split_manual.help   = pspm_cfg_help_format('pspm_split_sessions', 'options.splitpoints');
 
 %% Split behaviour
 split_behavior         = cfg_choice;
@@ -30,33 +29,27 @@ split_behavior.name    = 'Split behavior';
 split_behavior.tag     = 'split_behavior';
 split_behavior.values  = {split_auto, split_manual};
 split_behavior.val     = {split_auto};
-split_behavior.help    = {['Choose whether sessions should be detected ', ...
-'automatically or if sessions should be split according to ', ...
-'given marker id''s.']};
+split_behavior.help    = {};
 
 %% Missing epochs
 miss_epoch_false          = cfg_const;
 miss_epoch_false.name     = 'No missing epochs file';
 miss_epoch_false.tag      = 'no';
 miss_epoch_false.val      = {0};
-miss_epoch_false.help     = {'No missing epochs file to be processed.'};
+miss_epoch_false.help     = {};
 
 miss_epoch_true          = cfg_files;
 miss_epoch_true.name     = 'Add missing epochs file';
 miss_epoch_true.tag      = 'name';
 miss_epoch_true.num      = [1 1];
-miss_epoch_true.help     = {['The selected missing epochs file will be ',...
-'split as well.'], ['The input must be the name of a file containing missing ',...
-'epochs in seconds.']};
+miss_epoch_true.help     = {};
 
 missing_epoch         = cfg_choice;
 missing_epoch.name    = 'Missing epoch';
 missing_epoch.tag     = 'missing_epochs_file';
 missing_epoch.values  = {miss_epoch_false, miss_epoch_true};
 missing_epoch.val     = {miss_epoch_false};
-missing_epoch.help = {['A missing epochs file can be added here '...
-'and will be split in the same way as the PsPM data file. '...
-'Split sessions can handle up to one missing epoch file.']};
+missing_epoch.help    = pspm_cfg_help_format('pspm_split_sessions', 'options.missing');
 
 %% Executable branch
 split_sessions      = cfg_exbranch;
