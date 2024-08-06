4a50853e7586d52096459e2623ad08b4104a5553
Dominik Bach <dominik.bach@uzh.ch>
2024-08-06 15:19:56 +0200

    modify GUI for pspm_pp

--- src/pspm_cfg/pspm_cfg_filtering.m
+++ src/pspm_cfg/pspm_cfg_filtering.m
@@ -14,20 +14,22 @@ tau.name          = 'Time constant';
 tau.tag           = 'tau';
 tau.strtype       = 'r';
 tau.num           = [1 1];
-tau.help          = {'Time constant in seconds.'};
+tau.help          = pspm_cfg_help_format('pspm_pp', 'tau');
 
 FilterLeaky             = cfg_branch;
 FilterLeaky.name        = 'Leaky Integrator';
 FilterLeaky.tag         = 'leaky_integrator';
 FilterLeaky.val         = {tau};
 FilterLeaky.help        = {''};
+
 %% Medianfilter
 nr_time_pt               = cfg_entry;
 nr_time_pt.name          = 'Number of Time Points';
 nr_time_pt.tag           = 'nr_time_pt';
 nr_time_pt.strtype       = 'i';
 nr_time_pt.num           = [1 1];
-nr_time_pt.help          = {'Number of time points over which the median is taken.'};
+nr_time_pt.help          = pspm_cfg_help_format('pspm_pp', 'n');
+
 % Medianfilter
 FilterMedian             = cfg_branch;
 FilterMedian.name        = 'Median Filter';
@@ -39,13 +41,7 @@ filtertype               = cfg_choice;
 filtertype.name          = 'Filter Type';
 filtertype.tag           = 'filtertype';
 filtertype.values        = {FilterMedian,FilterButter,FilterLeaky};
-filtertype.help          = {['Currently, median and Butterworth ',...
-                           'filters and a leaky integrator are implemented. A median filter is ' ...
-                           'recommended for short spikes, generated ' ...
-                           'for example in MRI scanners by gradient ' ...
-                           'switching. A butterworth filter is already applied ' ...
-                           'in most psychophysiological models; check there to see whether ' ...
-                           'an additional filtering is meaningful. A leaky integrater is often used for EMG or neural data.']};
+filtertype.help          = {};
 
 %% Executable branch
 filtering             = cfg_exbranch;
@@ -55,12 +51,5 @@ filtering.tag         = 'filtering';
 filtering.val         = {datafile,chan_nr, chan_action, filtertype};
 filtering.prog        = @pspm_cfg_run_filtering;
 filtering.vout        = @pspm_cfg_vout_outchannel;
-filtering.help        = {['This module offers several basic filtering functions. ',...
-                           'Currently, a median filter and a butterworth low pass ' ...
-                           'filter are implemented. The median filter is useful to ' ...
-                           'remove short "spikes" in the data, for example from gradient ' ...
-                           'switching in MRI. The Butterworth filter can be used to get ' ...
-                           'rid of high frequency noise that is not sufficiently ',...
-                           'filtered away by the filters implemented on-the-fly during ',...
-                           'first level modelling.']};
+filtering.help        = pspm_cfg_help_format('pspm_pp');
 
