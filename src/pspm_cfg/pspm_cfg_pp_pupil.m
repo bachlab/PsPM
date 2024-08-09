b792b5a8251a7fbd724cf1288e84f4f08db21d09
Dominik Bach <dominik.bach@uzh.ch>
2024-08-09 16:50:07 +0200

    add gaze_pp to GUI

--- src/pspm_cfg/pspm_cfg_pp_pupil.m
+++ src/pspm_cfg/pspm_cfg_pp_pupil.m
@@ -13,6 +13,6 @@ function [cfg] = pspm_cfg_pp_pupil
 cfg        = cfg_repeat;
 cfg.name   = 'Pupil & Eye tracking';
 cfg.tag    = 'pp_pupil';
-cfg.values = {pspm_cfg_process_illuminance, pspm_cfg_find_valid_fixations, pspm_cfg_pupil_correct, pspm_cfg_pupil_preprocess, pspm_cfg_pupil_size_convert, pspm_cfg_gaze_convert};
+cfg.values = {pspm_cfg_process_illuminance, pspm_cfg_find_valid_fixations, pspm_cfg_pupil_correct, pspm_cfg_pupil_preprocess, pspm_cfg_pupil_size_convert, pspm_cfg_gaze_convert, pspm_cfg_gaze_pp};
 cfg.forcestruct = true;
 cfg.help   = {'Help: Pupil & Eye tracking preprocessing'};
