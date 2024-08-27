b13f8cd3026f3c67bba1fd21d033b85fd30d154c
Dominik Bach <dominik.bach@uzh.ch>
2024-08-27 14:42:55 +0200

    fix markerinfo in other functions

--- src/pspm_get_vario.m
+++ src/pspm_get_vario.m
@@ -59,7 +59,7 @@ for k = 1:numel(import)
     import{k}.data = [event.time];
     import{k}.marker = 'timestamp';
     import{k}.markerinfo.name = {event.name};
-    import{k}.markerinfo.value = {event.name};
+    import{k}.markerinfo.value = NaN(numel(event.name), 1);
   end
 end
 
