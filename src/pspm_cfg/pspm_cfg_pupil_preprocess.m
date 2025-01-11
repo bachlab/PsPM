bb1c2ee266f0af96256197089bc83095eb57b4e7
Bernhard <90285552+4gwe@users.noreply.github.com>
2025-01-11 16:13:44 +0100

    fix comment

--- src/pspm_cfg/pspm_cfg_pupil_preprocess.m
+++ src/pspm_cfg/pspm_cfg_pupil_preprocess.m
@@ -135,7 +135,7 @@ ResdFiltLPCF            = cfg_entry;
 ResdFiltLPCF.name       = 'Butterworth cutoff frequency (Hz)';
 ResdFiltLPCF.tag        = 'residualsFilter_interpFs';% should be residualsFilter_lowpassCF?
 ResdFiltLPCF.num        = [1 1];
-ResdFiltLPCF.val        = {16};% should be 1
+ResdFiltLPCF.val        = {16};% 
 ResdFiltLPCF.help       = pspm_cfg_help_format('pspm_pupil_pp_options', 'raw.residualsFilter_lowpassCF');
 
 % Keep filter data
