6023f4442f087fc99ba8130deefaec1aec06c33c
Bernhard <90285552+4gwe@users.noreply.github.com>
2025-03-07 12:24:56 +0100

    added mm to the helptext of pspm_pupil_pp_options

--- src/pspm_pupil_pp_options.m
+++ src/pspm_pupil_pp_options.m
@@ -15,9 +15,9 @@ function [sts, default_settings] = pspm_pupil_pp_options(custom_settings)
 %   │ 
 %   │ // Allowable values criteria
 %   │ 
-%   ├─PupilDiameter_Min:  Minimum allowable pupil size. Pupil values less than 
+%   ├─PupilDiameter_Min:  Minimum allowable pupil size in mm. Pupil values less than 
 %   │                     this value will be marked as invalid. (Default: 1.5 mm)
-%   ├─PupilDiameter_Max:  Maximum allowable pupil size. Pupil values greater than 
+%   ├─PupilDiameter_Max:  Maximum allowable pupil size in mm. Pupil values greater than 
 %   │                     thin value will be marked as invalid. (Default: 9.0 mm)
 %   │ 
 %   │ // Isolated sample filter criteria
