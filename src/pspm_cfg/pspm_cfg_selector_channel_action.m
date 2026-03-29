2773414f8867d3218b61b92fdb2ae470b919e3d8
4gwe <agoue.rau@gmail.com>
2026-03-29 19:53:22 +0200

    pspm_write_channel does no longer use units with pspm_select_channels and update to pspm_cfg_selector_channel_action helptext

--- src/pspm_cfg/pspm_cfg_selector_channel_action.m
+++ src/pspm_cfg/pspm_cfg_selector_channel_action.m
@@ -6,4 +6,4 @@ channel_action.tag  = 'channel_action';
 channel_action.values = {'add', 'replace'};
 channel_action.labels = {'Add', 'Replace'};
 channel_action.val = {'add'};
-channel_action.help = {'Choose whether to add a new channel, or to replace the last existing channel of the same type and with the same units (if any).'};
+channel_action.help = {'Choose whether to add a new channel, or to replace the processed channel.'};
