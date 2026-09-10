714208f4e3088933b650d7000707e3e1ecb8b987
4gwe <agoue.rau@gmail.com>
2026-09-10 12:34:57 +0200

    Test: Improve pspm_convert_hb2hp test coverage (#597)

--- test/pspm_convert_hb2hp_test.m
+++ test/pspm_convert_hb2hp_test.m
@@ -49,19 +49,19 @@ function invalid_input(this)
   this.verifyWarning(@() pspm_convert_hb2hp('abc'), 'ID:invalid_input');
   % Verify not a string sample rate
   this.verifyWarning(@() pspm_convert_hb2hp('abc','abc'), 'ID:invalid_input');
-  % Verify not a numeric channel
+  % Verify options is not a struct
   this.verifyWarning(@() pspm_convert_hb2hp('abc',2,'abc'), 'ID:invalid_input');
 
-  % 
-  % % Verify that call of pspm_load_data fails
-  % this.verifyWarning(@() pspm_convert_hb2hp(files{1},100), 'ID:nonexistent_file');
-  % % Verify that interpolation does not have enough points
-  % % this.verifyWarning(@() pspm_convert_hb2hp(files{2}, 100), 'ID:too_strict_limits');
-  % % Verify that call of pspm_write_channel fails
-  % options.channel_action = 'abc';
-  % this.verifyWarning(@() pspm_convert_hb2hp(files{3},100,[],options),'ID:invalid_input');
-  % %options.channel_action = 'add';
-  % %this.verifyWarningFree(@()pspm_convert_hb2hp(files{1},100,[],options));
+  fn = this.input_filename;
+
+  options.channel_action = 'abc';
+  this.verifyWarning( @() pspm_convert_hb2hp(fn, 100, options), 'ID:invalid_input');
+
+  options = struct('limit_lower', 2, 'limit_upper', 1);
+  this.verifyWarning( @() pspm_convert_hb2hp(fn, 100, options), 'ID:invalid_input');
+
+  options = struct('limit_lower', -1);
+  this.verifyWarning( @() pspm_convert_hb2hp(fn, 100, options), 'ID:invalid_input');
 
 end
 
@@ -77,22 +77,61 @@ function basic_conversion(this)
     this.verifyEqual(data{outchannel}.header.chantype, 'hp');
 end
 
+function selects_hb_channel_by_index(this)
+    fn = [tempname, '.mat'];
+
+    channels = { ...
+        struct('chantype', 'hb'), ...
+        struct('chantype', 'hb') ...
+        };
+
+    testdata = pspm_testdata_gen(channels, 5);
+
+    % Channel 1 -> HP = 1000 ms
+    testdata.data{1}.data = ...
+        [0.5; 1.5; 2.5; 3.5; 4.5];
+
+    % Channel 2 -> HP = 500 ms
+    testdata.data{2}.data = ...
+        [0.5; 1.0; 1.5; 2.0; 2.5; 3.0; 3.5; 4.0; 4.5];
+
+    save(fn, '-struct', 'testdata');
+    cleanup = onCleanup(@() delete(fn));
+
+    options.channel = 1;
+    options.channel_action = 'add';
+
+    [sts, outchannel] = pspm_convert_hb2hp(fn, 2, options);
+    this.verifyEqual(sts, 1);
+
+    [sts, ~, data] = pspm_load_data(fn);
+    this.verifyEqual(sts, 1);
+
+    hp = data{outchannel};
+
+    % Explicitly selecting channel 1 must produce 1000 ms.
+    this.verifyEqual(hp.data, 1000 * ones(size(hp.data)), 'AbsTol', 1e-10);
+end
+
 function too_strict_limits(this)
     fn = this.input_filename;
     sr = 1;
-    options = struct('limit_lower', 11, 'limit_upper', 11);
+    options = struct('limit_lower', 10, 'limit_upper', 11);
 
-    % options = struct('limit_lower', 10, 'limit_upper', 11);
-    % this.verifyWarning(@() pspm_convert_hb2hp(fn, sr, options), ...
-    % 'ID:too_strict_limits');
-
-    % this.verifyWarningFree(@() pspm_convert_hb2hp(this.input_filename, sr));
     [sts, outchannel] = pspm_convert_hb2hp(fn, sr,options);
     this.verifyEqual(sts, 1);
     [sts, infos, data, filestruct] = pspm_load_data(fn);
     this.verifyEqual(sts, 1);
     this.verifyEqual(data{outchannel}.header.chantype, 'hp');
-    this.verifyTrue(any(isnan(data{outchannel}.data)))
+    this.verifyTrue(all(isnan(data{outchannel}.data)));
+end
+
+function too_strict_limits_warning(this)
+    fn = this.input_filename;
+    sr = 1;
+
+    options = struct('limit_lower', 10, 'limit_upper', 11);
+    this.verifyWarning(@() pspm_convert_hb2hp(fn, sr, options), 'ID:too_strict_limits');
 end
 
 function add_replace_channel_action(this)
@@ -100,39 +139,134 @@ function add_replace_channel_action(this)
     sr = 1;
     options.channel_action = 'add';
 
-    [sts, infos, ~, filestruct] = pspm_load_data(fn);
+    [sts, ~, ~, filestruct] = pspm_load_data(fn);
     this.verifyEqual(sts, 1);
-    % display(infos.history)
 
-    % add 1st hp 
-    [sts, outchannel] = pspm_convert_hb2hp(fn, sr, options);
+    original_num_channels = filestruct.numofchan;
+
+    % add 1st hp
+    [sts, hp1] = pspm_convert_hb2hp(fn, sr, options);
     this.verifyEqual(sts, 1);
-    [sts, ~, data, ~ ] = pspm_load_data(fn);
+
+    [sts, ~, data, ~] = pspm_load_data(fn);
     this.verifyEqual(sts, 1);
-    this.verifyEqual(data{outchannel}.header.chantype, 'hp');
-    this.verifyEqual(filestruct.numofchan + 1 , numel(data));
-    % display(infos.history)
+    this.verifyEqual(data{hp1}.header.chantype, 'hp');
+    this.verifyEqual(original_num_channels + 1, numel(data));
 
     % add 2nd hp
-    [sts, outchannel] = pspm_convert_hb2hp(fn, sr, options);
+    [sts, hp2] = pspm_convert_hb2hp(fn, sr, options);
     this.verifyEqual(sts, 1);
-    [sts, ~, data, ~ ] = pspm_load_data(fn);
 
+    [sts, ~, data, ~] = pspm_load_data(fn);
     this.verifyEqual(sts, 1);
-    this.verifyEqual(data{outchannel}.header.chantype, 'hp');
-    this.verifyEqual(filestruct.numofchan + 2 , numel(data));
+    this.verifyEqual(data{hp2}.header.chantype, 'hp');
+    this.verifyEqual(original_num_channels + 2, numel(data));
 
+    % Make sure the two added channels are different channels
+    this.verifyNotEqual(hp1, hp2);
 
-    % replace hp (last)
-    options.channel_action = 'replace';    
-    [sts, outchannel] = pspm_convert_hb2hp(fn, sr, options);
+    % replace last hp
+    options.channel_action = 'replace';
+
+    [sts, hp_replaced] = pspm_convert_hb2hp(fn, sr, options);
     this.verifyEqual(sts, 1);
-    [sts, infos, data, ~] = pspm_load_data(fn);
+
+    [sts, ~, data, ~] = pspm_load_data(fn);
+    this.verifyEqual(sts, 1);
+
+    % Number of channels must stay the same
+    this.verifyEqual(original_num_channels + 2, numel(data));
+
+    % The last HP channel should have been replaced
+    this.verifyEqual(hp_replaced, hp2);
+
+    % Replaced channel is still HP
+    this.verifyEqual(data{hp_replaced}.header.chantype, 'hp');
+end
+
+function known_heart_period_values(this)
+    fn = [tempname, '.mat'];
+
+    duration = 5;
+    sr = 2;
+
+    channels = {struct('chantype', 'hb')};
+    testdata = pspm_testdata_gen(channels, duration);
+    testdata.data{1}.data = [0.5; 1.5; 3.0; 4.0];
+
+    save(fn, '-struct', 'testdata');
+    cleanup = onCleanup(@() delete(fn));
     
-    % infos.history
+    % Convert HB -> HP.
+    [sts, outchannel] = pspm_convert_hb2hp(fn, sr);
     this.verifyEqual(sts, 1);
-    this.verifyEqual(data{outchannel}.header.chantype, 'hp');
-    this.verifyEqual(filestruct.numofchan + 2 , numel(data));
+
+    [sts, ~, data, ~] = pspm_load_data(fn);
+    this.verifyEqual(sts, 1);
+
+    hp = data{outchannel};
+
+    % Expected interpolated HP signal.
+    expected_hp = [ ...
+        1000;
+        1000;
+        1000;
+        1166.666666666667;
+        1333.333333333333;
+        1500;
+        1250;
+        1000;
+        1000;
+        1000
+        ];
+
+    % Verify actual numerical conversion.
+    this.verifyEqual(hp.data, expected_hp, 'AbsTol', 1e-10);
+
+    % Verify metadata.
+    this.verifyEqual(hp.header.chantype, 'hp');
+    this.verifyEqual(hp.header.units, 'ms');
+    this.verifyEqual(hp.header.sr, sr);
+end
+
+function selects_last_hb_channel_by_default(this)
+    fn = [tempname, '.mat'];
+    
+    channels = { ...
+        struct('chantype', 'hb'), ...
+        struct('chantype', 'hb') ...
+        };
+
+    testdata = pspm_testdata_gen(channels, 5);
+
+    % Channel 1: IBI = 1 s -> HP = 1000 ms
+    testdata.data{1}.data = [0.5; 1.5; 2.5; 3.5; 4.5];
+
+    % Channel 2: IBI = 0.5 s -> HP = 500 ms
+    testdata.data{2}.data = ...
+        [0.5; 1.0; 1.5; 2.0; 2.5; 3.0; 3.5; 4.0; 4.5];
+
+    save(fn, '-struct', 'testdata');
+    cleanup = onCleanup(@() delete(fn));
+
+    sr = 2;
+    options.channel_action = 'add';
+
+    [sts, outchannel] = pspm_convert_hb2hp(fn, sr, options);
+
+    this.verifyEqual(sts, 1);
+
+    [sts, ~, data] = pspm_load_data(fn);
+    this.verifyEqual(sts, 1);
+
+    hp = data{outchannel};
+
+    this.verifyEqual(hp.header.chantype, 'hp');
+    this.verifyEqual(hp.header.units, 'ms');
+    this.verifyEqual(hp.header.sr, sr);
+
+    % Default 'hb' must select the LAST HB channel.
+    this.verifyEqual(hp.data, 500 * ones(size(hp.data)), 'AbsTol', 1e-10);
 
 end
 end
